74657cfd85110991a70c5f212b0e811313f662f0
Barry Brands <barrybrands02@hotmail.com>
2021-11-15 16:34:45 +0100

    Duplicated links removed

--- pwa/src/components/common/menu.tsx
+++ pwa/src/components/common/menu.tsx
@@ -40,15 +40,6 @@ export default function MainMenu() {
           <li className="utrecht-topnav__item">
             <Link to={'/entities'} className="utrecht-topnav__link" >Entities</Link>
           </li>
-          <li className="utrecht-topnav__item">
-            <Link to={'/data'} className="utrecht-topnav__link" >Data objects</Link>
-          </li>
-          <li className="utrecht-topnav__item" >
-            <Link to={'/sources'} className="utrecht-topnav__link" >Sources</Link>
-          </li>
-          <li className="utrecht-topnav__item" >
-            <Link to={'/entities'} className="utrecht-topnav__link" >Entities</Link>
-          </li>
           <li className="utrecht-topnav__item" >
             <Link to={'/endpoints'} className="utrecht-topnav__link" >Endpoints</Link>
           </li>
