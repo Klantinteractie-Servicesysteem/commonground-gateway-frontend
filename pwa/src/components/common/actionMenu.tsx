a571cdff58c6a43daff4d13333d9a12b176bc8df
Barry Brands <barrybrands02@hotmail.com>
2021-11-15 15:56:54 +0100

    Action menu fix

--- pwa/src/components/common/actionMenu.tsx
+++ pwa/src/components/common/actionMenu.tsx
@@ -8,16 +8,16 @@ export default function ActionMenu({pageDescription = null}) {
     <nav className="utrecht-sidenav">
       <ul className="utrecht-sidenav__list">
         <li className="utrecht-sidenav__item">
-          <a className="utrecht-sidenav__link" href="/products">Diensten</a><span></span>
+          <Link className="utrecht-sidenav__link" to="/products">Sources</Link><span></span>
         </li>
         <li className="utrecht-sidenav__item">
-          <a className="utrecht-sidenav__link" href="/cases">Mijn aanvragen</a><span></span>
+          <Link className="utrecht-sidenav__link" to="/cases">Entities</Link><span></span>
         </li>
         <li className="utrecht-sidenav__item">
-          <a className="utrecht-sidenav__link" href="/data">Mijn gegevens</a><span></span>
+          <Link className="utrecht-sidenav__link" to="/data">Data objects</Link><span></span>
         </li>
         <li className="utrecht-sidenav__item">
-          <a className="utrecht-sidenav__link" href="/vault">Mijn kluis</a><span></span>
+          <Link className="utrecht-sidenav__link" to="/logs">Logs</Link><span></span>
         </li>
 
       </ul>
