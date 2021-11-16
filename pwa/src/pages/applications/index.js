33c8400d7d345c6b0834237c76dda3f044114ae1
dev-hamid <gh.hamid89@gami.com>
2021-11-16 12:28:49 +0100

    maken table voor user , applications en configuration

---
+++ pwa/src/pages/applications/index.js
@@ -0,0 +1,48 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import Layout from "../../components/common/layout";
+import {useUrlContext} from "../../context/urlContext";
+
+
+const Applications = () => {
+  const context = useUrlContext();
+
+  return (
+    <Layout>
+      <main>
+        <div className="utrecht-html">
+          <table lang="nl" summary="Overzicht van de stemmen voor en tegen het betaald parkeren." className="">
+            <caption>Uitslag internetpeiling 8 juni tot en met 28 juni</caption>
+            <thead>
+            <tr>
+              <th scope="col">Username</th>
+              <th scope="col" className="numeric">E-mail</th>
+              <th scope="col" className="numeric"></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>Locatie A</td>
+              <td className="numeric">53</td>
+              <td className="numeric">113</td>
+            </tr>
+            <tr>
+              <td>Locatie B</td>
+              <td className="numeric">58</td>
+              <td className="numeric">42</td>
+            </tr>
+            <tr>
+              <td>Locatie C</td>
+              <td className="numeric">87</td>
+              <td className="numeric">16</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </main>
+    </Layout>
+
+  )
+}
+
+export default Applications
