9b36884281dc6f71add1a3e38978fc24c34c2aa0
Sarai Misidjan <Sarai@conduction.nl>
2021-11-22 17:58:47 +0100

    added codeblock select input

--- pwa/src/components/utility/selectInput.tsx
+++ pwa/src/components/utility/selectInput.tsx
@@ -1,6 +1,16 @@
 import * as _ from "lodash";
 import * as React from "react";
 
+/**
+ * This components handles select inputs.
+ *
+ * @param {object} options Either object used for the map or null.
+ * @param {string} target Target that holds the array data.
+ * @param {string|null} name Name used for the labels.
+ * @param {string|null} id Id used for the select and options.
+ * @param {object|null} data data for the check.
+ * @returns Jsx of the generated form.
+ */
 export const SelectInputComponent = ({options, target, id, data = null, name = null}) => {
   const selectOptions = options.map((option) =>
     data !== undefined &&
@@ -18,7 +28,7 @@ export const SelectInputComponent = ({options, target, id, data = null, name = n
   return (
     <>
       <span className="utrecht-form-label">{_.upperFirst(name ?? target)}</span>
-      <select name={name ?? target} id={id} className="utrecht-select utrecht-select--html-select">
+      <select name={target} id={id} className="utrecht-select utrecht-select--html-select">
         {
           selectOptions
         }
