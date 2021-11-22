661d567df9a2a6854d50baa48cdfe5ee740f9748
Sarai Misidjan <Sarai@conduction.nl>
2021-11-22 14:28:49 +0100

    fix select input component

--- pwa/src/components/utility/selectInput.tsx
+++ pwa/src/components/utility/selectInput.tsx
@@ -1,30 +1,24 @@
 import * as _ from "lodash";
 import * as React from "react";
 
-export const SelectInputComponent = ({ options, target, data = null, name = null }) => {
-  console.log(data)
-  const selectOptionsData = options.map((option) =>
+export const SelectInputComponent = ({options, target, data = null, name = null}) => {
+  const selectOptions = options.map((option) =>
     data !== undefined &&
     data !== null &&
-      data == option.name &&
-    <option selected value="string" id={target}>
-      {_.upperFirst(option.name)}
-    </option>
-  )
-
-  const selectOptions = options.map((option) =>
-    <option value="string" id={target}>
-      {_.upperFirst(option.name)}
-    </option>
+    data == option.name ?
+      <option selected value={option.value} id={target}>
+        {_.upperFirst(option.name)}
+      </option>
+      :
+      <option value={option.value} id={target}>
+        {_.upperFirst(option.name)}
+      </option>
   )
 
   return (
     <>
       <span className="utrecht-form-label">{_.upperFirst(name ?? target)}</span>
       <select name="type" id="typeInput" className="utrecht-select utrecht-select--html-select">
-        {
-          selectOptionsData
-        }
         {
           selectOptions
         }
