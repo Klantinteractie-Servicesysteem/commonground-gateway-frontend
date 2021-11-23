d23ef99fd8566fce8f754e1187d809b732ed5292
Sarai Misidjan <Sarai@conduction.nl>
2021-11-23 15:02:32 +0100

    added required, minLength and maxLength to the generic input component

--- pwa/src/components/utility/genericInput.tsx
+++ pwa/src/components/utility/genericInput.tsx
@@ -9,9 +9,12 @@ import * as React from "react";
  * @param {string|null} name Name used for the labels.
  * @param {string|null} id Id used for the input.
  * @param {object|null} data data for the check.
+ * @param {boolean|false} required if the input is required.
+ * @param {string|null} minLength the minLength of the input.
+ * @param {string|null} maxLength the maxLength of the input.
  * @returns Jsx of the generated form.
  */
-export const GenericInputComponent = ({type, target, id, data = null, name = null}) => {
+export const GenericInputComponent = ({type, target, id, data = null, name = null, required = false, minLength = null, maxLength = null }) => {
   return (
     <>
       <span className="utrecht-form-label">{_.upperFirst(name ?? target)}</span>
@@ -19,8 +22,11 @@ export const GenericInputComponent = ({type, target, id, data = null, name = nul
         className="utrecht-textbox utrecht-textbox--html-input"
         name={target}
         id={id}
-        defaultValue={data == null ? false : data}
+        defaultValue={data == null ? null : data}
         type={type}
+        required={required != false}
+        minLength={minLength == null ? false : minLength}
+        maxLength={maxLength == null ? false : maxLength}
       />
     </>
   );
