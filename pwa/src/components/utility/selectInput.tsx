843560d55e6c0cb82068c798e6145aea2664c067
Sarai Misidjan <Sarai@conduction.nl>
2021-11-23 14:19:20 +0100

    refactor selectInput

--- pwa/src/components/utility/selectInput.tsx
+++ pwa/src/components/utility/selectInput.tsx
@@ -12,27 +12,20 @@ import * as React from "react";
  * @returns Jsx of the generated form.
  */
 export const SelectInputComponent = ({options, target, id, data = null, name = null}) => {
-  const selectOptions = options.map((option) =>
-    data !== undefined &&
-    data !== null &&
-    data == option.name ?
-      <option selected value={option.value} id={target}>
-        {_.upperFirst(option.name)}
-      </option>
-      :
-      <option value={option.value} id={target}>
-        {_.upperFirst(option.name)}
-      </option>
-  )
-
   return (
     <>
       <span className="utrecht-form-label">{_.upperFirst(name ?? target)}</span>
       <select name={target} id={id} className="utrecht-select utrecht-select--html-select">
-        {
-          selectOptions
-        }
+        {options.map((option) => (
+          <option
+            selected={data == null ? false : data == option.name ? true : false}
+            value={option.value}
+            id={target}
+          >
+            {_.upperFirst(option.name)}
+          </option>
+        ))}
       </select>
     </>
-  )
-}
+  );
+};
