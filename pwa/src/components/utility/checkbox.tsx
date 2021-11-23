09020d0b1fa3c86b8ad68560ec313e43e7e39249
Sarai Misidjan <Sarai@conduction.nl>
2021-11-23 15:01:52 +0100

    fix checkbox component

--- pwa/src/components/utility/checkbox.tsx
+++ pwa/src/components/utility/checkbox.tsx
@@ -1,34 +1,19 @@
 import * as _ from "lodash";
 import * as React from "react";
 
-export const CheckboxComponent = ({type, id, defaultValue = null, data = null, nameLabel, nameAttribute}) => {
-
-  const checkboxData = () => {
-    return (
-        data ?
-        <input
-          className="form-check-input utrecht-checkbox utrecht-checkbox--html-input"
-          type={type}
-          id={id}
-          name={nameAttribute}
-          defaultChecked={true}
-          defaultValue={defaultValue}
-        />
-        :
-        <input
-          className="form-check-input utrecht-checkbox utrecht-checkbox--html-input"
-          type={type}
-          id={id}
-          name={nameAttribute}
-          defaultValue={defaultValue}
-        />
-    )
-  }
-
+export const CheckboxComponent = ({type, id, data = null, nameLabel, nameAttribute, required = false}) => {
   return (
     <>
       <div className="form-check">
-        { checkboxData }
+        <input
+          className="form-check-input utrecht-checkbox utrecht-checkbox--html-input"
+          type={type}
+          id={id}
+          name={nameAttribute}
+          defaultChecked={data == null ? false : true}
+          defaultValue={data == null ? "false" : "true"}
+          required={required == false ? false : true}
+        />
         <label className="form-check-label" htmlFor={id}>
           {_.upperFirst(nameLabel)}
         </label>
