bf93fec65ff25eef4b3e707857642e4a9cbfead2
dev-hamid <gh.hamid89@gami.com>
2021-11-24 15:37:30 +0100

    files changing to tsx ( do not merge)

--- pwa/src/components/common/modal.tsx
+++ pwa/src/components/common/modal.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import * as React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
 /**
@@ -18,27 +18,27 @@ export default function Modal({ id = 'firstModal', title = "ModalTitle", hideBut
     <>
       {
         hideButton === false &&
-        < button type="button" class="btn btn-primary" data-toggle="modal" data-target={"#" + id}>
+        < button type="button" className="btn btn-primary" data-toggle="modal" data-target={"#" + id}>
           Launch demo modal
         </button>
       }
-      <div class="modal fade" id={id} tabindex="-1" role="dialog" aria-labelledby={id + "Label"} aria-hidden="true">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id={id + "Label"}>Modal title</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <div className="modal fade" id={id} tableIndex="-1" role="dialog" aria-labelledby={id + "Label"} aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id={id + "Label"}>Modal title</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <div class="modal-body">
+            <div className="modal-body">
               {content}
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
               {
                 save === true &&
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
               }
             </div>
           </div>
