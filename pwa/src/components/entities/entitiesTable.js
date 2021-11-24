07f484a8cc425731a1079674f8d0cc472381ec98
Sarai Misidjan <Sarai@conduction.nl>
2021-11-24 12:39:05 +0100

    added table component to entity page

--- pwa/src/components/entities/entitiesTable.js
+++ pwa/src/components/entities/entitiesTable.js
@@ -1,13 +1,9 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
 import { useUrlContext } from "../../context/urlContext";
-import TableCells from "../common/tableCells";
 import Card from "../common/card";
-import { Link } from "gatsby";
-import Table from "../common/table";
-import TableHeaders from "../common/tableHeaders";
-import DeleteModal from "../modals/deleteModal";
 import Spinner from "../common/spinner";
+import Table from "../common/table";
 
 export default function EntitiesTable() {
   const [entities, setEntities] = React.useState(null);
@@ -44,94 +40,16 @@ export default function EntitiesTable() {
   }, []);
 
   return (
-    <Card title="Entities" modal="#helpModal" refresh={getEntities} add="/entities/new" >
-        <div className="row">
-          <div className="col-12">
-            {showSpinner === true ? (
-              <Spinner />
-            ) : (
-              <div className="utrecht-html">
-                <table
-                  lang="nl"
-                  summary="Overview of object entities fetched from the gateway."
-                  className="table"
-                >
-                  <TableHeaders
-                    headerItems={[
-                      {
-                        name: "Name",
-                      },
-                      { name: "Endpoint" },
-                      { name: "Route" },
-                      { name: "Source" },
-                      { name: "" },
-                    ]}
-                  />
-                  <tbody>
-                    {entities !== null && entities.length > 0 ? (
-                      entities.map((row) => (
-                        <TableCells
-                          cellItems={[
-                            { name: row.name },
-                            { name: row.endpoint },
-                            { name: row.route },
-                            { name: row.gateway.name },
-                            {
-                              renderItem: () => {
-                                return (
-                                  <>
-                                    <div className="d-flex">
-                                      <Link
-                                        className="ml-auto"
-                                        to={`/entities/${row.id}`}
-                                      >
-                                        <button className="utrecht-button btn-sm btn-success">
-                                          <i className="fas fa-edit pr-1"></i>
-                                          Edit
-                                        </button>
-                                      </Link>
-                                      <button
-                                        className="utrecht-button btn-sm btn-danger ml-2"
-                                        type="button"
-                                        data-bs-toggle="modal"
-                                        data-bs-target={`#item-${row.id.replaceAll(
-                                          "-",
-                                          ""
-                                        )}`}
-                                      >
-                                        <i className="fas fa-trash" />
-                                      </button>
-                                    </div>
-                                  </>
-                                );
-                              },
-                            },
-                          ]}
-                        />
-                      ))
-                    ) : (
-                      <TableCells
-                        cellItems={[
-                          { name: "No results found" },
-                          { name: "" },
-                          { name: "" },
-                          { name: "" },
-                          { name: "" },
-                        ]}
-                      />
-                    )}
-                  </tbody>
-                </table>
-                {entities !== null &&
-                  entities.map((item) => (
-                    <>
-                      <DeleteModal data={item} useFunction={getEntities} />
-                    </>
-                  ))}
-              </div>
-            )}
-          </div>
+    <Card title="Entities" modal="#helpModal" refresh={getEntities} add="/entities/new">
+      <div className="row">
+        <div className="col-12">
+          {showSpinner === true ? (
+            <Spinner />
+          ) : (
+            <Table properties={[{ th: "Name", property: "name" }, { th: "Endpoint", property: "endpoint" }, { th: "Route", property: "route" }, { th: "Source", property: "gateway.name" }]} items={entities} editLink="/entities" />
+          )}
         </div>
+      </div>
     </Card>
   );
 }
