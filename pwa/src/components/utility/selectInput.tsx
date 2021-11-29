d3c3c9b9607e14ee1da28dec3fc3d127cbda57a0
Sarai Misidjan <Sarai@conduction.nl>
2021-11-29 13:58:41 +0100

    edited select input

--- pwa/src/components/utility/selectInput.tsx
+++ pwa/src/components/utility/selectInput.tsx
@@ -2,33 +2,43 @@ import * as _ from "lodash";
 import * as React from "react";
 
 interface SelectInputProps {
-  options: Record<any, any>
-  name: string
-  nameOverride?: string
-  id: string
-  data?: string
+  options: Array<Record<"value"|"name", any>>;
+  name: string;
+  nameOverride?: string;
+  id: string;
+  data?: string;
+  required?: boolean;
 }
 
 /**
- * This components handles select inputs.
- * @param {object} props The needed properties from the selectInputProps.
+ * This component generates a select input.
  * @returns Jsx of the generated form.
  */
 export const SelectInputComponent = (props: SelectInputProps) => {
   return (
     <>
-      <label className="utrecht-form-label" htmlFor={props.id}>{_.upperFirst(props.name ?? props.nameOverride)}</label>
-      <select name={props.name} id={props.id} className="utrecht-select utrecht-select--html-select">
+      <div className="input-group">
+      <label className="utrecht-form-label" htmlFor={props.id}>{_.upperFirst(props.nameOverride ?? props.name)}</label>
+      <select name={props.name} id={props.id} required={props.required} className="utrecht-select utrecht-select--html-select">
+        {
+          !props.required &&
+            <option> </option>
+        }
         {props.options.map((option) => (
           <option
-            selected={props.data == null ? false : props.data == option.name ? true : false}
-            value={props.data == null ? null : props.data}
-            id={props.name}
+            key={option.value}
+            selected={props.data == null ? false : props.data === option.value}
+            value={option.value}
           >
             {_.upperFirst(option.name)}
           </option>
         ))}
       </select>
+      </div>
     </>
   );
+};
+
+SelectInputComponent.defaultProps = {
+  required: false,
 };
