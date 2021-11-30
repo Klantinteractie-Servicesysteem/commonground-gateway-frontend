78b9c89a9ae3db54a2b7d118e6d1aba9a92af122
Sarai Misidjan <Sarai@conduction.nl>
2021-11-30 11:45:14 +0100

    edited tabs component

--- pwa/src/components/common/tabs.tsx
+++ pwa/src/components/common/tabs.tsx
@@ -1,14 +1,15 @@
 import * as React from "react";
 
+interface TabsProps {
+  items: Array<Partial<Record<"name"|"active"|"id", any>>>;
+}
 
 /**
  * This components renders tabs.
- *
- * @param {array} items Each item has a id (used for switching/opening tabs), name and active (if tab is active it is shown) property
  * @returns TSX of the generated tabs.
  */
-export default function Tabs({ items = null }) {
-    const navItems = items.map((item) =>
+export default function Tabs(props: TabsProps) {
+    const navItems = props.items.map((item) =>
         <li className="nav-item" role="presentation" key={item.name}>
           <a className={item.active !== undefined && item.active ? 'nav-link active' : 'nav-link'} id={item.id + "-tab"} data-bs-toggle="tab" data-bs-target={"#"+item.id}
             role="tab" aria-controls={item.id} aria-selected="true">{item.name}
