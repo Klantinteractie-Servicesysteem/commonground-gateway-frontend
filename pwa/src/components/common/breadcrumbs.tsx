4cf4ea0216c8bfdbbf0960bc1cbbcdbcc1c1b978
Sarai Misidjan <Sarai@conduction.nl>
2021-11-30 12:49:04 +0100

    edited breadcrumbs component

--- pwa/src/components/common/breadcrumbs.tsx
+++ pwa/src/components/common/breadcrumbs.tsx
@@ -1,15 +1,17 @@
 import * as React from "react";
 import { Link } from "gatsby";
 
+interface BreadcrumbsProps {
+  items: Array<Partial<Record<"name"|"href", any>>>;
+}
 
 /**
  * This components renders bootstrap breadcrumbs.
  *
- * @param {object} items Each item is a breadcrumb.
  * @returns TSX of the generated breadcrumbs.
  */
-export default function Breadcrumbs({ items = null }) {
-    const liItems = items.map((item) =>
+export default function Breadcrumbs(props: BreadcrumbsProps) {
+    const liItems = props.items.map((item) =>
       <li className="utrecht-breadcrumb__item">
         <Link className="utrecht-breadcrumb__link utrecht-breadcrumb__link--focus utrecht-link"
           to={item.href}>
