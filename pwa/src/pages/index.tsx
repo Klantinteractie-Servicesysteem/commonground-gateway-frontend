d03b06c571164d83c13825703ae6fe20cb10646b
Sarai Misidjan <Sarai@conduction.nl>
2021-12-14 12:54:23 +0100

    added tables and modals

--- pwa/src/pages/index.tsx
+++ pwa/src/pages/index.tsx
@@ -1,20 +1,51 @@
 import * as React from "react";
 import Layout from "../components/common/layout";
-import { useUrlContext } from "../context/urlContext";
+import {Tabs}from "@conductionnl/nl-design-system/lib/Tabs/src/tabs";
+import ResponseTable from "../components/logs/responseTable";
+import RequestTable from "../components/logs/requestTable";
 
 const IndexPage = () => {
-  const context = useUrlContext();
 
   return (
     <Layout
       title={"Dashboard"}
       subtext={"Welcome to the gateway admin dashboard"}
     >
-      <a href={`${context.apiUrl}/export/all`} target="_blank">
-        <button className="utrecht-button" type="button">
-          Export Configuration
-        </button>
-      </a>
+      <Tabs items={[{
+        name: 'Response logs',
+        id: 'response',
+        active: true
+      },{
+        name: 'Request logs',
+        id: 'request'
+      }
+      ]}/>
+
+      <div className="tab-content">
+        <div
+          className="tab-pane active"
+          id="response"
+          role="tabpanel"
+          aria-labelledby="main-tab"
+        >
+          <br />
+         <ResponseTable />
+        </div>
+        <div
+          className="tab-pane"
+          id="request"
+          role="tabpanel"
+          aria-labelledby="attributes-tab"
+        >
+          <br />
+          <RequestTable />
+        </div>
+      </div>
+      {/*<a href={`${context.apiUrl}/export/all`} target="_blank">*/}
+      {/*  <button className="utrecht-button" type="button">*/}
+      {/*    Export Configuration*/}
+      {/*  </button>*/}
+      {/*</a>*/}
     </Layout>
   );
 };
