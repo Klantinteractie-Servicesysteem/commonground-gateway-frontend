8bdf18c34238774b7f7e1ddc573b1e8597a13423
Sarai Misidjan <Sarai@conduction.nl>
2021-12-14 16:33:31 +0100

    fix application page

---
+++ pwa/src/components/applications/applicationForm.js
@@ -0,0 +1,108 @@
+import * as React from "react";
+import Spinner from "../common/spinner";
+import Card from "../common/card";
+import {GenericInputComponent} from "@conductionnl/nl-design-system/lib/GenericInput/src/genericInput";
+import {isLoggedIn} from "../../services/auth";
+
+export default function ApplicationForm({ id }) {
+  const [context, setContext] = React.useState(null);
+  let id = props.params.id;
+
+  const [application, setApplication] = React.useState(null);
+  const [showSpinner, setShowSpinner] = React.useState(false);
+  const [title, setTitle] = React.useState("Application");
+
+  React.useEffect(() => {
+    if (typeof window !== "undefined" && context === null) {
+      setContext({
+        apiUrl: window.GATSBY_API_URL,
+        frontendUrl: window.GATSBY_FRONTEND_URL,
+      });
+    } else {
+      if (isLoggedIn()) {
+        if (id !== "new") {
+          fetch(context.apiUrl + "/applications/" + id, {
+            credentials: 'include',
+            headers: {'Content-Type': 'application/json'},
+          })
+            .then(response => response.json())
+            .then((data) => {
+              setApplication(data);
+            });
+        }
+      }
+    }
+  }, [context]);
+
+  const saveApplication = () => {
+    setShowSpinner(true);
+
+    let url = context.apiUrl + '/applications';
+    let method = 'POST';
+    if (id !== 'new') {
+      url = url + '/' + id;
+      method = 'PUT';
+    }
+
+    let nameInput = document.getElementById('nameInput');
+    let descriptionInput = document.getElementById('descriptionInput');
+
+
+    let body = {
+      name: nameInput.value,
+      description: descriptionInput.value,
+
+    }
+
+    fetch(url, {
+      method: method,
+      credentials: 'include',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(body)
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log('Saved applications:', data);
+        setApplication(data);
+        setShowSpinner(false);
+        setTitle(data.name);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  return (
+    <form id="dataForm" onSubmit={saveApplication}>
+      <Card title="Values" back="/sources" save={true} >
+        <div className="row">
+          <div className="col-12">
+            {showSpinner === true ? (
+              <Spinner />
+            ) : (
+              <>
+                <div className="row">
+                  <div className="col-6">
+                    {application !== null && application.name !== null ? (
+                      <GenericInputComponent type={"text"} name={"name"} id={"nameInput"} data={application.name} nameOverride={"Name"} required={"true"}/>
+                    ) : (
+                      <GenericInputComponent type={"text"} name={"name"} id={"nameInput"}  nameOverride={"Name"} required={"true"}/>
+                    )}
+                  </div>
+                  <div className="col-6">
+                    {application !== null && application.description !== null ? (
+                      <GenericInputComponent nameOverride={"Description"} name={"description"} data={application.description} type={"text"} required={"true"} id={"descriptionInput"}/>
+                    ) : (
+                      <GenericInputComponent nameOverride={"Description"} name={"description"} type={"text"} required={"true"} id={"descriptionInput"}/>
+                    )}
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+
+      </Card>
+    </form>
+  );
+}
--- pwa/src/pages/applications/[id].js
+++ pwa/src/pages/applications/[id].js
@@ -2,145 +2,17 @@ import * as React from "react";
 import Layout from "../../components/common/layout";
 import {Link} from "gatsby";
 import { isLoggedIn } from "../../services/auth";
-
+import ObjectEntityForm from "../../components/object_entities/object_entityForm";
+import ApplicationForm from "../../components/applications/applicationForm";
 
 const IndexPage = (props) => {
-  const [context, setContext] = React.useState(null);
-  let id = props.params.id;
-
-  const [application, setApplication] = React.useState(null);
-  const [showSpinner, setShowSpinner] = React.useState(false);
-  const [title, setTitle] = React.useState("Application");
-
-  React.useEffect(() => {
-    if (typeof window !== "undefined" && context === null) {
-      setContext({
-        apiUrl: window.GATSBY_API_URL,
-        frontendUrl: window.GATSBY_FRONTEND_URL,
-      });
-    } else {
-      if (isLoggedIn()) {
-        if (id !== "new") {
-          fetch(context.apiUrl + "/applications/" + id, {
-            credentials: 'include',
-            headers: {'Content-Type': 'application/json'},
-          })
-            .then(response => response.json())
-            .then((data) => {
-              setApplication(data);
-            });
-        }
-      }
-    }
-  }, [context]);
-
-  const saveApplication = () => {
-    setShowSpinner(true);
-
-    let url = context.apiUrl + '/applications';
-    let method = 'POST';
-    if (id !== 'new') {
-      url = url + '/' + id;
-      method = 'PUT';
-    }
-
-    let nameInput = document.getElementById('nameInput');
-    let descriptionInput = document.getElementById('descriptionInput');
-
-
-    let body = {
-      name: nameInput.value,
-      description: descriptionInput.value,
-
-    }
-
-    fetch(url, {
-      method: method,
-      credentials: 'include',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(body)
-    })
-      .then(response => response.json())
-      .then((data) => {
-        console.log('Saved applications:', data);
-        setApplication(data);
-        setShowSpinner(false);
-        setTitle(data.name);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  }
 
   return (
-    <Layout title={title} subtext={"Edit your application here"}>
+    <Layout>
       <main>
         <div className="row">
           <div className="col-12">
-            <div className="utrecht-card card">
-              <div className="utrecht-card-header card-header">
-                <div className="utrecht-card-head-row card-head-row row">
-                  <div className="col-6">
-                    <h4 className="utrecht-heading-4 utrecht-heading-4--distanced utrecht-card-title">Values</h4>
-                  </div>
-                  <div className="col-6 text-right">
-                    <Link className="utrecht-link" to="/applications">
-                      <button className="utrecht-button utrecht-button-sm btn-sm btn-danger mr-2"><i
-                        className="fas fa-long-arrow-alt-left mr-2"></i>Back
-                      </button>
-                    </Link>
-                    <a className="utrecht-link" onClick={saveApplication}>
-                      <button className="utrecht-button utrecht-button-sm btn-sm btn-success"><i
-                        className="fas fa-save mr-2"></i>Save
-                      </button>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="utrecht-card-body card-body">
-                <div className="row">
-                  <div className="col-12">
-                    {
-                      showSpinner == true ?
-                        <div className="text-center py-5">
-                          <div class="spinner-border text-primary" style={{width: "3rem", height: "3rem"}}
-                               role="status">
-                            <span class="sr-only">Loading...</span>
-                          </div>
-                        </div> :
-                        <form id="dataForm" onSubmit={saveApplication}>
-                          <div className="row">
-                            <div className="col-6">
-                              <div className="form-group">
-                                <span className="utrecht-form-label mb-2">Name *</span>
-                                {
-                                  application !== null && application.name !== null ?
-                                    <input className="utrecht-textbox utrecht-textbox--html-input" name="name"
-                                           id="nameInput" defaultValue={application.name} required/> :
-                                    <input className="utrecht-textbox utrecht-textbox--html-input" name="name"
-                                           id="nameInput" required/>
-                                }
-                              </div>
-                            </div>
-                            <div className="col-6">
-                              <div className="form-group">
-                                <span className="utrecht-form-label">description</span>
-                                {
-                                  application !== null && application.description !== null ?
-                                    <input className="utrecht-textbox utrecht-textbox--html-input" name="description"
-                                           id="descriptionInput" defaultValue={application.description} /> :
-                                    <input className="utrecht-textbox utrecht-textbox--html-input" name="description"
-                                           id="descriptionInput" />
-                                }
-                              </div>
-                            </div>
-                          </div>
-                        </form>
-                    }
-                  </div>
-                </div>
-              </div>
-            </div>
+            <ApplicationForm id={props.params.id}/>
           </div>
         </div>
       </main>
