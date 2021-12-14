ad02a60f1d8a62c86d710048b68e24bb8f967f62
Sarai Misidjan <Sarai@conduction.nl>
2021-12-14 16:26:17 +0100

    fix import

--- pwa/src/pages/applications/[id].js
+++ pwa/src/pages/applications/[id].js
@@ -1,6 +1,8 @@
-import * as React from "react"
+import * as React from "react";
 import Layout from "../../components/common/layout";
-import {Link} from "gatsby"
+import {Link} from "gatsby";
+import { isLoggedIn } from "../../services/auth";
+
 
 const IndexPage = (props) => {
   const [context, setContext] = React.useState(null);
