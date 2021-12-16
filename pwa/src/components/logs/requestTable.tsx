b00347362587c9861ba5e25a71ffdd46ab81f973
ginokok1996 <ginokok1996@hotmail.com>
2021-12-16 09:39:53 +0100

    updates to dashboard page

--- pwa/src/components/logs/requestTable.tsx
+++ pwa/src/components/logs/requestTable.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
-import {Table} from "@conductionnl/nl-design-system/lib/Table/src/table";
-import {isLoggedIn} from "../../services/auth";
+import { Table } from "@conductionnl/nl-design-system/lib/Table/src/table";
+import { isLoggedIn } from "../../services/auth";
 import Modal from "@conductionnl/nl-design-system/lib/Modal/src/modal";
+import Spinner from "../common/spinner";
+import { Card } from "@conductionnl/nl-design-system";
 
 export default function RequestTable() {
   const [context, setContext] = React.useState(null);
   const [requests, setRequest] = React.useState(null);
+  const [showSpinner, setShowSpinner] = React.useState(false);
 
   React.useEffect(() => {
     if (typeof window !== "undefined" && context === null) {
@@ -18,90 +21,140 @@ export default function RequestTable() {
   }, [context]);
 
   const getRequests = () => {
+    setShowSpinner(true);
     fetch(`${context.adminUrl}/request_logs`, {
-      credentials: 'include',
-      headers: {"Content-Type": "application/json", 'Authorization': 'Bearer ' + sessionStorage.getItem('jwt')},
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + sessionStorage.getItem("jwt"),
+      },
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data["hydra:member"])
         setRequest(data["hydra:member"]);
+        setShowSpinner(false);
       });
   };
 
   return (
     <>
+      <Card
+        title="Request Logs"
+        cardHeader={function () {
+          return (
+            <>
+              <a className="utrecht-link" onClick={getRequests}>
+                <i className="fas fa-sync-alt" />
+              </a>
+            </>
+          );
+        }}
+        cardBody={() => {
+          return (
+            <>
+              {showSpinner && <Spinner />}
+              {showSpinner == false && (
+                <>
+                  {requests !== null && requests.length > 0 ? (
+                    <Table
+                      columns={[
+                        {
+                          headerName: "Status",
+                          field: "status",
+                        },
+                        {
+                          headerName: "Status Code",
+                          field: "statusCode",
+                        },
+                        {
+                          headerName: "Method",
+                          field: "method",
+                        },
+                        {
+                          field: "id",
+                          headerName: " ",
+                          renderCell: (item) => {
+                            return (
+                              <div className="float-right mr-4">
+                                <button
+                                  type="button"
+                                  className="btn btn-primary"
+                                  data-bs-toggle="modal"
+                                  data-bs-target={`#requestLogs${item.id.replaceAll(
+                                    "-",
+                                    ""
+                                  )}`}
+                                >
+                                  Request logs
+                                </button>
+                              </div>
+                            );
+                          },
+                        },
+                      ]}
+                      rows={requests}
+                    />
+                  ) : (
+                    <Table
+                      columns={[
+                        {
+                          headerName: "Status",
+                          field: "status",
+                        },
+                        {
+                          headerName: "Status Code",
+                          field: "statusCode",
+                        },
+                        {
+                          headerName: "Method",
+                          field: "method",
+                        },
+                      ]}
+                      rows={[]}
+                    />
+                  )}
+                </>
+              )}
+            </>
+          );
+        }}
+      />
+
       {requests !== null &&
-      requests.length > 0 ? (
-        <Table columns={[{
-          headerName: "Status",
-          field: "status"
-        }, {
-          headerName: "Status Code",
-          field: "statusCode"
-        }, {
-          headerName: "Method",
-          field: "method"
-        }, {
-          field: "id",
-          headerName: " ",
-          renderCell: (item) => {
-            return (
-              <div className="float-right mr-4">
-                <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target={`#requestLogs${item.id.replaceAll('-', '')}`}>
-                  Request logs
-                </button>
-              </div>
-            );
-          },
-        }]} rows={requests}/>
-      ) : (
-        <Table columns={[{
-          headerName: "Status",
-          field: "status"
-        }, {
-          headerName: "Status Code",
-          field: "statusCode"
-        }, {
-          headerName: "Method",
-          field: "method"
-        }]} rows={[]}/>
-      )}
-
-
-      { requests !== null &&
         requests.map((request) => (
-        <Modal title={"Request Logs"}
-               id={`requestLogs${request.id}`}
-               body={function () {
-                 return (
-                   <div>
-                     {
-                       request.responseBody.path !== null && (
-                         <>
-                           <p><b>Path: </b>{request.responseBody.path}</p>
-                         </>
-                       )
-                     }
-                     {
-                       request.responseBody.type !== null && (
-                         <>
-                           <p><b>Type: </b>{request.responseBody.type}</p>
-                         </>
-                       )
-                     }
-                     {
-                       request.responseBody.message !== null && (
-                         <>
-                           <b>Message</b>
-                           <p>{request.responseBody.message}</p>
-                         </>
-                       )
-                     }
-                   </div>
-                 )
-               }}/>
-      ))}
+          <Modal
+            title={"Request Logs"}
+            id={`requestLogs${request.id}`}
+            body={function () {
+              return (
+                <div>
+                  {request.responseBody.path !== null && (
+                    <>
+                      <p>
+                        <b>Path: </b>
+                        {request.responseBody.path}
+                      </p>
+                    </>
+                  )}
+                  {request.responseBody.type !== null && (
+                    <>
+                      <p>
+                        <b>Type: </b>
+                        {request.responseBody.type}
+                      </p>
+                    </>
+                  )}
+                  {request.responseBody.message !== null && (
+                    <>
+                      <b>Message</b>
+                      <p>{request.responseBody.message}</p>
+                    </>
+                  )}
+                </div>
+              );
+            }}
+          />
+        ))}
     </>
   );
 }
--- pwa/src/components/logs/responseTable.tsx
+++ pwa/src/components/logs/responseTable.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
-import {Table} from "@conductionnl/nl-design-system/lib/Table/src/table";
-import {isLoggedIn} from "../../services/auth";
+import { Table } from "@conductionnl/nl-design-system/lib/Table/src/table";
+import { isLoggedIn } from "../../services/auth";
 import Modal from "@conductionnl/nl-design-system/lib/Modal/src/modal";
+import Spinner from "../common/spinner";
+import { Card } from "@conductionnl/nl-design-system";
 
 export default function ResponseTable() {
   const [context, setContext] = React.useState(null);
   const [responses, setResponse] = React.useState(null);
+  const [showSpinner, setShowSpinner] = React.useState(false);
 
   React.useEffect(() => {
     if (typeof window !== "undefined" && context === null) {
@@ -18,89 +21,140 @@ export default function ResponseTable() {
   }, [context]);
 
   const getResponses = () => {
+    setShowSpinner(true);
     fetch(`${context.adminUrl}/response_logs`, {
-      credentials: 'include',
-      headers: {"Content-Type": "application/json", 'Authorization': 'Bearer ' + sessionStorage.getItem('jwt')},
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + sessionStorage.getItem("jwt"),
+      },
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data["hydra:member"])
         setResponse(data["hydra:member"]);
+        setShowSpinner(false);
       });
   };
 
   return (
     <>
-      {responses !== null &&
-      responses > 0 ? (
-        <Table columns={[{
-          headerName: "Status",
-          field: "status"
-        }, {
-          headerName: "Status Code",
-          field: "statusCode"
-        }, {
-          headerName: "Method",
-          field: "method"
-        }, {
-          field: "id",
-          headerName: " ",
-          renderCell: (item) => {
-            return (
-              <div className="float-right mr-4">
-                <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target={`#responseLogs${item.id.replaceAll('-', '')}`}>
-                  Response logs
-                </button>
-              </div>
-            );
-          },
-        }]} rows={responses}/>
-      ) : (
-        <Table columns={[{
-          headerName: "Status",
-          field: "status"
-        }, {
-          headerName: "Status Code",
-          field: "statusCode"
-        }, {
-          headerName: "Method",
-          field: "method"
-        }]} rows={[]}/>
-      )}
+      <Card
+        title="Response Logs"
+        cardHeader={function () {
+          return (
+            <>
+              <a className="utrecht-link" onClick={getResponses}>
+                <i className="fas fa-sync-alt" />
+              </a>
+            </>
+          );
+        }}
+        cardBody={() => {
+          return (
+            <>
+              {showSpinner && <Spinner />}
+              {showSpinner == false && (
+                <>
+                  {responses !== null && responses > 0 ? (
+                    <Table
+                      columns={[
+                        {
+                          headerName: "Status",
+                          field: "status",
+                        },
+                        {
+                          headerName: "Status Code",
+                          field: "statusCode",
+                        },
+                        {
+                          headerName: "Method",
+                          field: "method",
+                        },
+                        {
+                          field: "id",
+                          headerName: " ",
+                          renderCell: (item) => {
+                            return (
+                              <div className="float-right mr-4">
+                                <button
+                                  type="button"
+                                  className="btn btn-primary"
+                                  data-bs-toggle="modal"
+                                  data-bs-target={`#responseLogs${item.id.replaceAll(
+                                    "-",
+                                    ""
+                                  )}`}
+                                >
+                                  Response logs
+                                </button>
+                              </div>
+                            );
+                          },
+                        },
+                      ]}
+                      rows={responses}
+                    />
+                  ) : (
+                    <Table
+                      columns={[
+                        {
+                          headerName: "Status",
+                          field: "status",
+                        },
+                        {
+                          headerName: "Status Code",
+                          field: "statusCode",
+                        },
+                        {
+                          headerName: "Method",
+                          field: "method",
+                        },
+                      ]}
+                      rows={[]}
+                    />
+                  )}
+                </>
+              )}
+            </>
+          );
+        }}
+      />
 
-      { responses !== null &&
-      responses.map((response) => (
-        <Modal title={"Response Logs"}
-               id={`responseLogs${response.id}`}
-               body={function () {
-                 return (
-                   <div>
-                     {
-                       response.responseBody.path !== null && (
-                         <>
-                           <p><b>Path: </b>{response.responseBody.path}</p>
-                         </>
-                       )
-                     }
-                     {
-                       response.responseBody.type !== null && (
-                         <>
-                           <p><b>Type: </b>{response.responseBody.type}</p>
-                         </>
-                       )
-                     }
-                     {
-                       response.responseBody.message !== null && (
-                         <>
-                           <b>Message</b>
-                           <p>{response.responseBody.message}</p>
-                         </>
-                       )
-                     }
-                   </div>
-                 )
-               }}/>
-      ))}
+      {responses !== null &&
+        responses.map((response) => (
+          <Modal
+            title={"Response Logs"}
+            id={`responseLogs${response.id}`}
+            body={function () {
+              return (
+                <div>
+                  {response.responseBody.path !== null && (
+                    <>
+                      <p>
+                        <b>Path: </b>
+                        {response.responseBody.path}
+                      </p>
+                    </>
+                  )}
+                  {response.responseBody.type !== null && (
+                    <>
+                      <p>
+                        <b>Type: </b>
+                        {response.responseBody.type}
+                      </p>
+                    </>
+                  )}
+                  {response.responseBody.message !== null && (
+                    <>
+                      <b>Message</b>
+                      <p>{response.responseBody.message}</p>
+                    </>
+                  )}
+                </div>
+              );
+            }}
+          />
+        ))}
     </>
   );
 }
