de3f2f9de838511fcd4b77f2ee15c546d56c7749
Barry Brands <barrybrands02@hotmail.com>
2021-12-17 14:24:45 +0100

    SourceTable import improved

--- pwa/src/components/sources/sourcesTable.tsx
+++ pwa/src/components/sources/sourcesTable.tsx
@@ -1,9 +1,8 @@
 import * as React from "react";
 import Spinner from "../common/spinner";
-import { Card } from "@conductionnl/nl-design-system/lib/Card/src/card";
 import { isLoggedIn } from "../../services/auth";
 import { Link } from "gatsby";
-import { Table } from "@conductionnl/nl-design-system/lib/Table/src/table";
+import { Table, Card } from "@conductionnl/nl-design-system/lib";
 
 export default function SourcesTable() {
   const [sources, setSources] = React.useState(null);
