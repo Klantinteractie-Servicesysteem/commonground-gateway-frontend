3b21f43949e7a2e51cc03349d539ebbe6ea11c85
Lennart <66552932+lencodes@users.noreply.github.com>
2022-01-10 17:19:33 +0100

    REFACTOR ElementCreationNew to store value in state and disable button accordingly

--- pwa/src/components/common/elementCreationNew.tsx
+++ pwa/src/components/common/elementCreationNew.tsx
@@ -2,28 +2,23 @@ import * as React from 'react'
 
 interface Value {
   id: string,
-  key?: string,
   value: string,
 }
 
 const ElementCreationNew: React.FC = () => {
   const valueInputEl = React.useRef(null)
+  const [value, setValue] = React.useState<string>()
   const [values, setValues] = React.useState<Value[]>([])
 
   const handleAdd = () => {
-    if (!valueInputEl.current.value) return
-
     setValues([...values, { id: Math.random().toString(36), value: valueInputEl.current.value}])
-
-    valueInputEl.current.value = null // empty input after processing
+    setValue('') // cleanup after processing
   }
 
-  const handleDelete = (e) => {
-    setValues(values.filter(value => value.id !== e.target.id))
-  }
+  const handleDelete = (e) => { setValues(values.filter(value => value.id !== e.target.id)) }
 
   return (
-    <>
+    <div>
       {values.length > 0 &&
         <ul>
           {values.map((value, idx) => {
@@ -37,9 +32,9 @@ const ElementCreationNew: React.FC = () => {
         </ul>
       }
 
-      <input ref={valueInputEl} type="text" />
-      <button onClick={handleAdd}>Add</button>
-    </>
+      <input value={value} onChange={(e) => setValue(e.target.value)} ref={valueInputEl} type="text" />
+      <button onClick={handleAdd} disabled={value.length < 1}>Add</button>
+    </div>
   )
 }
 
