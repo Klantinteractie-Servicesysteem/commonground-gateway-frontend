3797a49067aae3501bed4eb0ea5227b1c7833d72
Lennart <66552932+lencodes@users.noreply.github.com>
2022-01-13 15:45:30 +0100

    REFACTOR process.env

--- pwa/src/components/sources/sourcesTable.tsx
+++ pwa/src/components/sources/sourcesTable.tsx
@@ -14,7 +14,7 @@ export default function SourcesTable() {
   React.useEffect(() => {
     if (typeof window !== "undefined" && context === null) {
       setContext({
-        adminUrl: window['GATSBY_ADMIN_URL']
+        adminUrl: process.env.GATSBY_ADMIN_URL
       });
     } else if (isLoggedIn()) {
       getSources();
