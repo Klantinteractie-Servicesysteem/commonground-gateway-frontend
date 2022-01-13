4d3e997d5b2d1016b29138fb25058bf8dc688597
Lennart <66552932+lencodes@users.noreply.github.com>
2022-01-13 13:00:54 +0100

    REFACTOR removes redundant inputElRef

--- pwa/src/components/common/elementCreationNew.tsx
+++ pwa/src/components/common/elementCreationNew.tsx
@@ -1,18 +1,17 @@
 import * as React from 'react'
 
-interface Value {
+interface IValue {
   id: string,
   value: string,
 }
 
 const ElementCreationNew: React.FC = () => {
-  const valueInputEl = React.useRef(null)
-  const [value, setValue] = React.useState<string>()
-  const [values, setValues] = React.useState<Value[]>([])
+  const [value, setValue] = React.useState<string>("")
+  const [values, setValues] = React.useState<IValue[]>([])
 
   const handleAdd = () => {
-    setValues([...values, { id: Math.random().toString(36), value: valueInputEl.current.value}])
-    setValue('') // cleanup after processing
+    setValues([...values, { id: Math.random().toString(36), value: value }])
+    setValue("")
   }
 
   const handleDelete = (e) => { setValues(values.filter(value => value.id !== e.target.id)) }
@@ -20,20 +19,20 @@ const ElementCreationNew: React.FC = () => {
   return (
     <div>
       {values.length > 0 &&
-      <ul>
-        {values.map((value, idx) => {
-          return (
-            <li key={idx}>
-              {value.value}
-              <button id={value.id} onClick={handleDelete}>Delete</button>
-            </li>
-          )
-        })}
-      </ul>
+        <ul>
+          {values.map((value, idx) => {
+            return (
+              <li key={idx}>
+                {value.value}
+                <button id={value.id} onClick={handleDelete}>Delete</button>
+              </li>
+            )
+          })}
+        </ul>
       }
 
-      <input value={value} onChange={(e) => setValue(e.target.value)} ref={valueInputEl} type="text" />
-      <button onClick={handleAdd}>Add</button>
+      <input value={value} onChange={(e) => setValue(e.target.value)} type="text" />
+      <button onClick={handleAdd} disabled={!value}>Add</button>
     </div>
   )
 }
