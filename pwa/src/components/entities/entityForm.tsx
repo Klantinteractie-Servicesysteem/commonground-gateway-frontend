6e9f0f09f2ccff5fbdd9d949f085185b2807849f
Hamid <gh.hamid89@gmail.com>
2022-01-19 09:52:22 +0100

    ElementCreationNew implementeer entitiyform

--- pwa/src/components/entities/entityForm.tsx
+++ pwa/src/components/entities/entityForm.tsx
@@ -1,29 +1,28 @@
 import * as React from "react";
-import {
-  GenericInputComponent,
-  Checkbox,
-  SelectInputComponent,
-  Accordion,
-  Card,
-  Alert
-}
-  from "@conductionnl/nl-design-system/lib";
-import {isLoggedIn} from "../../services/auth";
-import {navigate} from "gatsby-link";
 import {Link} from "gatsby";
-import Spinner from "../common/spinner";
-import FlashMessage from 'react-flash-message';
 import {
   checkValues,
-  removeEmptyObjectValues, retrieveFormArrayAsOArray,
+  removeEmptyObjectValues,
+  retrieveFormArrayAsOArray,
 } from "../utility/inputHandler";
-import {ArrayInputComponent} from "../common/arrayInput";
+import {
+  GenericInputComponent,
+  Accordion,
+  Card,
+  Alert,
+  Spinner,
+  Checkbox,
+  SelectInputComponent
+} from "@conductionnl/nl-design-system/lib";
+import FlashMessage from 'react-flash-message';
+import {isLoggedIn} from "../../services/auth";
+import ElementCreationNew from "../common/elementCreationNew"
+
 
 export default function EntityForm({id}) {
   const [context, setContext] = React.useState(null);
   const [showSpinner, setShowSpinner] = React.useState<boolean>(false);
   const [alert, setAlert] = React.useState<any>(null);
-
   const [entity, setEntity] = React.useState<any>(null);
   const [sources, setSources] = React.useState<any>(null);
   const [soaps, setSoaps] = React.useState<any>(null);
@@ -113,59 +112,43 @@ export default function EntityForm({id}) {
     let availableProperties = retrieveFormArrayAsOArray(event.target, "availableProperties");
     let collectionConfig = retrieveFormArrayAsOArray(event.target, "collectionConfig");
 
-    let body: {} = {
+    let body = {
       name: event.target.name.value,
-      description: event.target.description.value ? event.target.description.value : null,
-      route: event.target.route.value ? event.target.route.value : null,
-      endpoint: event.target.endpoint.value ? event.target.endpoint.value : null,
+      description: event.target.description.value
+        ? event.target.description.value : null,
+      route: event.target.route.value
+        ? event.target.route.value : null,
+      endpoint: event.target.endpoint.value
+        ? event.target.endpoint.value : null,
       gateway: event.target.gateway.value
-        ? event.target.gateway.value
-        : null,
+        ? event.target.gateway.value : null,
       extend: event.target.extend.checked,
-      function: event.target.function.value ? event.target.function.value : null,
+      function: event.target.function.value
+        ? event.target.function.value : null,
+      transformations,
+      translationConfig,
+      usedProperties,
+      availableProperties,
+      collectionConfig,
     };
 
-    // check arrays
-    if (transformations.length !== 0) {
-      body["transformations"] = transformations;
-    } else {
-      body["transformations"] = [];
-    }
-
-    if (translationConfig.length !== 0) {
-      body["translationConfig"] = translationConfig;
-    } else {
-      body["translationConfig"] = [];
-    }
-
-    if (usedProperties.length !== 0) {
-      body["usedProperties"] = usedProperties;
-    } else {
-      body["usedProperties"] = [];
-    }
-
-    if (availableProperties.length !== 0) {
-      body["availableProperties"] = availableProperties;
-    } else {
-      body["availableProperties"] = [];
-    }
-
-    if (collectionConfig.length !== 0) {
-      body["collectionConfig"] = collectionConfig;
-    } else {
-      body["collectionConfig"] = [];
-    }
 
     // This removes empty values from the body
     body = removeEmptyObjectValues(body);
 
-    if (!checkValues([body["name"]])) {
+
+    if (!checkValues([body.name])) {
+      setAlert(null);
+      setAlert({type: 'danger', message: 'Required fields are empty'});
+      setShowSpinner(false);
       return;
     }
 
     let url = `${context.adminUrl}/entities`;
-    let method = "POST";
-    if (id !== "new") {
+    let method = null;
+    if (id === "new") {
+      method = "POST";
+    } else {
       url = `${url}/${id}`;
       method = "PUT";
     }
@@ -178,17 +161,17 @@ export default function EntityForm({id}) {
     })
       .then((response) => response.json())
       .then((data) => {
+        console.log(data)
         setShowSpinner(false);
-        setEntity(data);
-        method === 'POST' && navigate(`/entities`)
+        setEntity(data)
       })
       .catch((error) => {
         setShowSpinner(false);
-        console.error(error);
+        console.log("Error:", error);
         setAlert(null);
         setAlert({type: 'danger', message: error.message});
       });
-  }
+  };
 
   return (<>
       {
@@ -216,7 +199,8 @@ export default function EntityForm({id}) {
                       <i className="fas fa-save mr-2"/>Save
                     </button>
                   </div>
-                )}}
+                )
+              }}
               cardBody={function () {
                 return (
                   <div className="row">
@@ -227,12 +211,14 @@ export default function EntityForm({id}) {
                         <div>
                           <div className="row">
                             <div className="col-6">
-                                <GenericInputComponent type={"text"} name={"name"} id={"nameInput"} data={entity && entity.name && entity.name}
-                                                       nameOverride={"Name"} required/>
+                              <GenericInputComponent type={"text"} name={"name"} id={"nameInput"}
+                                                     data={entity && entity.name && entity.name}
+                                                     nameOverride={"Name"} required/>
                             </div>
                             <div className="col-6">
-                                <GenericInputComponent type={"text"} name={"description"} id={"descriptionInput"}
-                                                       data={entity && entity.description && entity.description} nameOverride={"Description"}/>
+                              <GenericInputComponent type={"text"} name={"description"} id={"descriptionInput"}
+                                                     data={entity && entity.description && entity.description}
+                                                     nameOverride={"Description"}/>
                             </div>
                           </div>
                           <div className="row">
@@ -253,13 +239,14 @@ export default function EntityForm({id}) {
                           </div>
                           <div className="row">
                             <div className="col-6">
-                                <GenericInputComponent type={"text"} name={"endpoint"} id={"endpointInput"}
-                                                       data={entity && entity.endpoint && entity.endpoint}
-                                                       nameOverride={"Endpoint"}/>
+                              <GenericInputComponent type={"text"} name={"endpoint"} id={"endpointInput"}
+                                                     data={entity && entity.endpoint && entity.endpoint}
+                                                     nameOverride={"Endpoint"}/>
                             </div>
                             <div className="col-6">
-                                <GenericInputComponent type={"text"} name={"route"} id={"routeInput"}
-                                                       data={entity && entity.route && entity.route} nameOverride={"Route"}/>
+                              <GenericInputComponent type={"text"} name={"route"} id={"routeInput"}
+                                                     data={entity && entity.route && entity.route}
+                                                     nameOverride={"Route"}/>
                             </div>
                           </div>
                           <div className="row">
@@ -324,92 +311,60 @@ export default function EntityForm({id}) {
                               </div>
                             </div>
                           </div>
-                          {/* FromSoap TODO */}
-                          {/* <div className="row">
-                      <div className="col-12">
-                        <div className="form-group">
-                          <ArrayInput />
-                        </div>
-                      </div>
-                    </div> */}
                           <div className="row">
                             <div className="col-12">
                               <div className="form-check">
-                                      <Checkbox type={"checkbox"} id={"extendInput"}
-                                                nameLabel={"Extend"} nameAttribute={"extend"}
-                                                data={entity && entity.extend && entity.extend}/>
+                                <Checkbox type={"checkbox"} id={"extendInput"}
+                                          nameLabel={"Extend"} nameAttribute={"extend"}
+                                          data={entity?.extend}/>
                               </div>
                             </div>
                           </div>
                           <Accordion
                             id="entityAccordion"
-                            items={[
-                              {
-                                title: "Transformations",
-                                id: "transformationsAccordion",
-                                render: function () {
-                                  return (
-                                    <>
-                                        <ArrayInputComponent
-                                          id={"transformations"}
-                                          label={"Transformations"}
-                                          data={entity && entity.transformations ? entity.transformations : null}
-                                        />
-                                    </>
-                                  );
-                                },
-                              },
-                              {
-                                title: "Translation Config",
-                                id: "translationConfigAccordion",
-                                render: function () {
-                                  return (
-                                        <ArrayInputComponent
-                                          id={"translationConfig"}
-                                          label={"Translation Config"}
-                                          data={entity && entity.translationConfig ? entity.translationConfig : null}
-                                        />
-                                  );
-                                },
-                              },
+                            items={[{
+                              title: "Transformations",
+                              id: "transformationsAccordion",
+                              render: function () {
+                                return <ElementCreationNew
+                                  id="transformations"
+                                  label="Transformations"
+                                  data={entity?.transformations}
+                                />
+                              }
+                            },
                               {
                                 title: "Collection Config",
                                 id: "collectionConfigAccordion",
                                 render: function () {
-                                  return (
-                                        <ArrayInputComponent
-                                          id={"collectionConfig"}
-                                          data={entity && entity.collectionConfig ? entity.collectionConfig : null}
-                                          label={"Collection Config"}
-                                        />
-                                  )
+                                  return <ElementCreationNew
+                                    id="collectionConfig"
+                                    label="Collection Config"
+                                    data={entity?.collectionConfig}
+                                  />
                                 }
                               },
                               {
                                 title: "Used Properties",
                                 id: "usedPropertiesAccordion",
                                 render: function () {
-                                  return (
-                                        <ArrayInputComponent
-                                          id={"usedProperties"}
-                                          label={"Used Properties"}
-                                          data={entity && entity.usedProperties ? entity.usedProperties : null}
-                                        />
-                                  );
-                                },
+                                  return <ElementCreationNew
+                                    id="usedProperties"
+                                    label="Used Properties"
+                                    data={entity?.usedProperties}
+                                  />
+                                }
                               },
                               {
                                 title: "Available Properties",
                                 id: "availablePropertiesAccordion",
                                 render: function () {
-                                  return (
-                                        <ArrayInputComponent
-                                          id={"availableProperties"}
-                                          label={"Available Properties"}
-                                          data={entity && entity.availableProperties ? entity.availableProperties : null}
-                                        />
-                                  );
-                                },
+                                  return <ElementCreationNew
+                                    id="availableProperties"
+                                    label="Available Properties"
+                                    data={entity?.availableProperties}
+                                  />
+                                }
                               }
                             ]}
                           />
