e0c23ee43eea39686276fc5e6fd66bc5e1f2f071
Lennart <66552932+lencodes@users.noreply.github.com>
2022-01-24 13:59:24 +0100

    REFACTOR apiService for build errors

--- pwa/src/apiService/apiService.ts
+++ pwa/src/apiService/apiService.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
+const windowGlobal = typeof window !== 'undefined' && window
+
 export const axiosClient = axios.create({
   baseURL: process.env.GATSBY_ADMIN_URL,
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
-    Authorization: "Bearer " + sessionStorage?.getItem("jwt"),
+    Authorization: "Bearer " + windowGlobal.sessionStorage.getItem("jwt"),
   }
 });
