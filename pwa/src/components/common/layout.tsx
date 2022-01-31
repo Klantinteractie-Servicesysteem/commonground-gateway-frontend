a4b15654444dec729138261d7e0e636779452b26
Lennart <66552932+lencodes@users.noreply.github.com>
2022-01-31 11:41:13 +0100

    ADD new Footer component and IMPLEMENT in Layout

--- pwa/src/components/common/layout.tsx
+++ pwa/src/components/common/layout.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import Footer from "./footer";
+import Footer from './../footer/footer'
 import MainMenu from "./menu";
 import { Helmet } from "react-helmet";
 import "bootstrap/dist/css/bootstrap.css";
