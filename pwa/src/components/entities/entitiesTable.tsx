9c9ab7426162811b58a9b00b2416c69de2d2b8b2
Hamid <gh.hamid89@gmail.com>
2022-01-31 16:12:10 +0100

    edit PR "Route" hernoemen naar "Path"

--- pwa/src/components/entities/entitiesTable.tsx
+++ pwa/src/components/entities/entitiesTable.tsx
@@ -70,7 +70,7 @@ export default function EntitiesTable() {
                       field: "endpoint",
                     },
                     {
-                      headerName: "Route",
+                      headerName: "Path",
                       field: "route",
                     },
                     {
