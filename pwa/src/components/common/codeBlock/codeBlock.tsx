12785f88ccc38c525f0afb043c6679495df4e5f5
Barry Brands <barrybrands02@hotmail.com>
2022-02-01 12:08:14 +0100

    Indentation codeBlock.tsx

--- pwa/src/components/common/codeBlock/codeBlock.tsx
+++ pwa/src/components/common/codeBlock/codeBlock.tsx
@@ -2,18 +2,18 @@ import * as React from "react";
 import './codeBlock.css';
 
 interface CodeBlockProps {
-    body: any | null
-    type?: string | null
+  body: any,
+  type?: string | null
 }
 
 export const CodeBlock: React.FC<CodeBlockProps> = ({ body = null, type = 'json' }) => {
 
-    return (
-        <pre className="codeblock-pre">
-            <code id="responseContent" className={"language-" + (type && type + ' ' + 'codeblock-pre-color-' + type)}>
-                {body}
-            </code>
-        </pre>
-    );
+  return (
+    <pre className="codeblock-pre">
+      <code id="responseContent" className={"language-" + (type && type + ' ' + 'codeblock-pre-color-' + type)}>
+        {body}
+      </code>
+    </pre>
+  );
 }
 export default CodeBlock;
