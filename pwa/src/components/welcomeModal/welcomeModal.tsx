510b7a427b41a5f2e9207024c891b13f22036867
Lennart <66552932+lencodes@users.noreply.github.com>
2022-02-03 10:41:54 +0100

    REFACTOR welcomeModal component to more React way of working

--- pwa/src/components/welcomeModal/welcomeModal.tsx
+++ pwa/src/components/welcomeModal/welcomeModal.tsx
@@ -1,47 +1,41 @@
 
 import * as React from "react";
-// import { Modal } from "@conductionnl/nl-design-system/lib";
-import {Modal} from "./modal";
 import {navigate} from "gatsby";
 import APIService from "../../apiService/apiService";
 import APIContext from "../../apiService/apiContext";
+import { Modal } from "@conductionnl/nl-design-system";
 
-const WelcomeModal = () => {
+const WelcomeModal: React.FC = () => {
   const API: APIService = React.useContext(APIContext);
-  const modalRef = React.useRef(null);
-  
-  React.useEffect(() => {
-    console.log(modalRef.current);
-  }, [modalRef.current])
+  const buttonRef = React.useRef(null);
 
   React.useEffect(() => {
-    ClickButtonForModal(0);
+    renderOptionalWelcomeModal()
   }, [API])
 
-  const ClickButtonForModal = (appCount) => {
-    let button = document.getElementById('welcomeModalButton');
-    if (button != null && appCount == 0) {
-      // button.click();
-    }
+  const renderOptionalWelcomeModal = (): void => {
+    API.Application.getAll()
+      .then((res) => {
+        res.data.length === 4 && buttonRef.current.click()
+      })
+      .catch((err) => { throw new Error('GET applications error: ' + err) })
   }
 
-  const ApplicationsCount = () => {
-    API.Application.getAll()
-      .then((res) => { return res.data.length })
-      .catch((err) => { throw new Error('GET applications error: ' + err) })
-  };
-
-  const navToApplications = () => {
-    navigate('/applications/new');
-  };
-
-  const welcomeModalBody = () => {
-    return <>
-      <p>Welcome to the gateway user-interface.</p>
-      <p>It seems you haven't created a application yet, would you like to
-        <a className="utrecht--link" data-bs-dismiss="modal" aria-label="Close" onClick={navToApplications}> create an application </a>
-        first or you can close this modal if you want to find things out yourself. </p>
-    </>
+  const welcomeModalBody = (): JSX.Element => {
+    return (
+      <>
+        <p>Welcome to the gateway user-interface.</p>
+        <p>It seems you haven't created a application yet, would you like to
+          <a
+            className="utrecht--link"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+            onClick={() => { navigate('/applications/new') }}>
+              {` create an application `}
+          </a>
+          first or you can close this modal if you want to find things out yourself. </p>
+      </>
+    )
   };
 
   return (<>
@@ -49,22 +43,14 @@ const WelcomeModal = () => {
       type="button"
       className="btn btn-primary"
       data-bs-toggle="modal"
-      data-bs-target={`#welcomeModal`}
+      data-bs-target="#welcomeModal"
       style={{ display: 'none' }}
-      id="welcomeModalButton"
+      ref={buttonRef}
     >
       More info
     </button>
-    <Modal title={'Hello!'} id='welcomeModal' body={welcomeModalBody} ref={modalRef} />
+    <Modal title={'Hello!'} id='welcomeModal' body={welcomeModalBody} />
   </>);
 };
 
 export default WelcomeModal;
-
-
-
-
-
-
-
-
