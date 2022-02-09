e9779853a4d1d14203338d5281a35d8c2c41e57c
Sarai Misidjan <Sarai@conduction.nl>
2022-02-09 10:45:55 +0100

    added  redirect for a null or expired jwt token

--- pwa/src/components/common/layout.tsx
+++ pwa/src/components/common/layout.tsx
@@ -6,10 +6,11 @@ import "bootstrap/dist/css/bootstrap.css";
 import Header from "./header";
 import { APIProvider } from "../../apiService/apiContext";
 import APIService from "../../apiService/apiService";
-import { isLoggedIn } from "../../services/auth";
+import {isLoggedIn, logout} from "../../services/auth";
 import Login from "../../pages/login";
 import WelcomeModal from "../welcomeModal/welcomeModal";
 import favicon from "../../images/conduction_logo_blauw.svg";
+import {navigate} from "gatsby-link";
 
 /**
  * This components renders a layout which is renders the menu, footer and container surrounding main body of pages.
@@ -22,9 +23,26 @@ import favicon from "../../images/conduction_logo_blauw.svg";
 export default function Layout({ children, title = "", subtext = "" }) {
   const [API, setAPI] = React.useState<APIService>(null)
 
+  const parseJwt = (token) => {
+    try {
+      return JSON.parse(atob(token.split(".")[1]));
+    } catch (e) {
+      return null;
+    }
+  };
+  const decodedJwt = parseJwt(sessionStorage.getItem('jwt'));
+
   React.useEffect(() => {
+    if (decodedJwt === null) {
+      logout()
+      navigate("/");
+    } else if (decodedJwt.exp * 1000 < Date.now()) {
+      logout();
+      navigate("/");
+    }
+
     !API && setAPI(new APIService(sessionStorage.getItem('jwt')))
-  }, [API])
+  }, [API, decodedJwt])
 
   return (
     API &&
