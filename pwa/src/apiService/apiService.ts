f8ce23e0f39a53b6a4506c4e5378b8673f62a93e
Barry Brands <barrybrands02@hotmail.com>
2022-02-10 15:07:31 +0100

    gatsby env improvements

--- pwa/src/apiService/apiService.ts
+++ pwa/src/apiService/apiService.ts
@@ -6,7 +6,7 @@ import Entity from './resources/entity';
 import ObjectEntity from "./resources/ObjectEntity";
 import Login from './services/login';
 import Log from './resources/log';
-import Endpoint from "./resources/endpoint";
+import { GATSBY_ADMIN_URL, GATSBY_API_URL } from '../../static/env.js';
 
 export default class APIService {
   private _jwtToken: string;
@@ -17,7 +17,7 @@ export default class APIService {
 
   public get adminClient (): AxiosInstance {
     return axios.create({
-      baseURL: window.GATSBY_ADMIN_URL,
+      baseURL: GATSBY_ADMIN_URL,
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
@@ -28,7 +28,7 @@ export default class APIService {
 
   public get apiClient (): AxiosInstance {
     return axios.create({
-      baseURL: window.GATSBY_API_URL,
+      baseURL: GATSBY_API_URL,
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
@@ -43,7 +43,7 @@ export default class APIService {
   public get Entity (): Entity { return new Entity(this.adminClient) }
   public get ObjectEntity (): ObjectEntity { return new ObjectEntity(this.adminClient) }
   public get Log (): Log { return new Log(this.adminClient) }
-  public get Endpoint (): Endpoint { return new Endpoint(this.adminClient) }
+  public get Translation (): Translation { return new Translation(this.adminClient) }
 
   // Services
   public get Login (): Login { return new Login(this.apiClient) }
