e88254d23d474d08f95ee1c7e73d0770cf539b2f
Lennart <66552932+lencodes@users.noreply.github.com>
2022-02-15 15:10:51 +0100

    IMPLEMENT DashboardCard--small in Dashboard Template

--- pwa/src/templates/dashboard/Dashboard.tsx
+++ pwa/src/templates/dashboard/Dashboard.tsx
@@ -1,13 +1,16 @@
 import * as React from "react";
 import "./dashboard.css"
-import { DashboardCard } from "./../../components/dashboardCard/DashboardCard"
+import { DashboardCard, DashboardCardSmall } from "./../../components/dashboardCard/DashboardCard"
 import CallHealthQuickview from "./../../components/callHealthQuickview/CallHealthQuickview"
 import DashboardLogsTable from "../../components/dashboardLogsTable/DashboardLogsTable";
 import { DashboardLogs } from "../../dummy_data/dashboardLog";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
 
 import applicationsIcon from "./../../images/icon-applications.svg";
 import sourcesIcon from "./../../images/icon-sources.svg";
 import endpointsIcon from "./../../images/icon-endpoints.svg";
+import conductionIcon from "./../../images/icon-conduction.svg";
 
 const Dashboard: React.FC = () => {
   return (
@@ -29,7 +32,6 @@ const Dashboard: React.FC = () => {
           <DashboardCard
             amount={4}
             title="Applications"
-            linkType="internal"
             iconBackgroundColor="6861CE"
             icon={<img src={applicationsIcon} alt="applications" />}
             subtitle={<CallHealthQuickview healthyCallsAmount={3} unhealthyCallsAmount={1} />}
@@ -39,7 +41,6 @@ const Dashboard: React.FC = () => {
           <DashboardCard
             amount={16}
             title="Sources"
-            linkType="internal"
             iconBackgroundColor="FFAD46"
             icon={<img src={sourcesIcon} alt="sources" />}
             subtitle={<CallHealthQuickview healthyCallsAmount={18} unhealthyCallsAmount={3} />}
@@ -49,7 +50,6 @@ const Dashboard: React.FC = () => {
           <DashboardCard
             amount={88}
             title="Endpoints"
-            linkType="internal"
             iconBackgroundColor="31CE36"
             icon={<img src={endpointsIcon} alt="endpoints" />}
             subtitle="View all endpoints"
@@ -72,7 +72,25 @@ const Dashboard: React.FC = () => {
         </div>
 
         <div className="dashboard-externalLinks">
-          Documentation and support
+          <h3 className="dashboard-dividerTitle">
+            Documentation and support
+          </h3>
+
+          <DashboardCardSmall
+            title="Documentation"
+            subtitle="View the Read The Docs"
+            iconBackgroundColor="979797"
+            icon={<FontAwesomeIcon icon={faBook} />}
+            linkTo="https://commonground-gateway.readthedocs.io/en/latest/"
+          />
+
+          <DashboardCardSmall
+            title="Conduction"
+            subtitle="Get in touch"
+            iconBackgroundColor="4276FC"
+            icon={<img src={conductionIcon} alt="conduction" />}
+            linkTo="https://conduction.nl"
+          />
         </div>
       </div>
     </div>
