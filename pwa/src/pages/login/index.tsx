e2a001dabae0bd1e8262d14ce962be9daa388e00
Lennart <66552932+lencodes@users.noreply.github.com>
2022-02-17 14:52:46 +0100

    BUILD FIX

--- pwa/src/pages/login/index.tsx
+++ pwa/src/pages/login/index.tsx
@@ -38,7 +38,54 @@ const Login: React.FC = () => {
 
   return (
     <div className="login">
-      <Particles options={ParticlesOptions} />
+      <Particles
+        options={
+          {
+            fpsLimit: 60,
+            particles: {
+              color: {
+                value: "#ffffff",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outMode: "bounce",
+                random: false,
+                speed: 2,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                random: true,
+                value: 5,
+              },
+            },
+            detectRetina: true,
+          }
+        }
+      />
 
       <div className="login-container">
         <h1>Welcome to your gateway admin dashboard</h1>
