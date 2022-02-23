12f35296399b0201405424ff281410f262323466
Hamid <gh.hamid89@gmail.com>
2022-02-23 14:35:43 +0100

    PR Attributes tabel refactor

--- pwa/src/components/common/LabelWithBackground.tsx
+++
@@ -1,8 +0,0 @@
-interface LabelWithBackgroundProps {
-  label: string;
-  type: "success" | "danger" | "primary" | "secondary";
-}
-
-const LabelWithBackground: React.FC<LabelWithBackgroundProps> = () => {
-  return <></>; // create the component here
-};
