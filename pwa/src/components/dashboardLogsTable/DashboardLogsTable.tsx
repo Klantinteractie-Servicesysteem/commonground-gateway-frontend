1ee977e505525473391c81b16cc92077c927951b
Lennart <66552932+lencodes@users.noreply.github.com>
2022-03-04 08:33:13 +0100

    REFACTOR DashboardLogsTable LogModal to be outside table

--- pwa/src/components/dashboardLogsTable/DashboardLogsTable.tsx
+++ pwa/src/components/dashboardLogsTable/DashboardLogsTable.tsx
@@ -6,7 +6,6 @@ import LogModal from "../logs/logModal/LogModal";
 import LabelWithBackground from "../LabelWithBackground/LabelWithBackground";
 import msToSeconds from "../../services/msToSeconds";
 
-
 interface DashboardLogsTableProps {
   logs: any;
 }
@@ -16,43 +15,48 @@ const DashboardLogsTable: React.FC<DashboardLogsTableProps> = ({ logs }) => {
     <div className="dashboardLogsTable">
       <table>
         <thead>
-        <tr>
-          <th>Status</th>
-          <th>Type</th>
-          <th>Method</th>
-          <th>Response time</th>
-        </tr>
+          <tr>
+            <th>Status</th>
+            <th>Type</th>
+            <th>Method</th>
+            <th>Response time</th>
+          </tr>
         </thead>
         <tbody>
-        {!logs.length && <tr><td>No results found</td></tr> }
-        {logs.map((log, idx) => {
-          const statusClass = log.responseStatusCode > 199 && log.responseStatusCode < 300 ? "success" : "danger";
+          {!logs.length && (
+            <tr>
+              <td>No results found</td>
+            </tr>
+          )}
+          {logs.map((log, idx) => {
+            const statusClass = log.responseStatusCode > 199 && log.responseStatusCode < 300 ? "success" : "danger";
             return (
-              <>
-                <tr key={idx} className="dashboardLogsTable-tr">
-                  <td>
-                    <LabelWithBackground label={log.responseStatusCode.toString()} type={statusClass} />
-                  </td>
-                  <td>{log.type}</td>
-                  <td>{log.requestMethod}</td>
-                  <td>{`${log.responseTime}ms (${msToSeconds(log.responseTime)}s)`}</td>
-                  <td className="dashboardLogsTable-viewLogTd">
-                    <button
-                      type="button"
-                      className="btn btn-primary"
-                      data-bs-toggle="modal"
-                      data-bs-target={`#logs${log.id.replace(new RegExp("-", "g"), "")}`}
-                    >
-                      <FontAwesomeIcon icon={faEye} /> View log
-                    </button>
-                  </td>
-                </tr>
-              <LogModal {...{ log }} />
-            </>
-          );
-        })}
+              <tr key={idx} className="dashboardLogsTable-tr">
+                <td>
+                  <LabelWithBackground label={log.responseStatusCode.toString()} type={statusClass} />
+                </td>
+                <td>{log.type}</td>
+                <td>{log.requestMethod}</td>
+                <td>{`${log.responseTime}ms (${msToSeconds(log.responseTime)}s)`}</td>
+                <td className="dashboardLogsTable-viewLogTd">
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    data-bs-toggle="modal"
+                    data-bs-target={`#logs${log.id.replace(new RegExp("-", "g"), "")}`}
+                  >
+                    <FontAwesomeIcon icon={faEye} /> View log
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
+
+      {logs.map((log) => (
+        <LogModal key={log} {...{ log }} />
+      ))}
     </div>
   );
 };
