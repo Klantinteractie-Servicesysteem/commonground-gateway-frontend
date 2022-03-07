116bc2ac0dcf6100c82932f81065133e8853c64e
Lennart <66552932+lencodes@users.noreply.github.com>
2022-03-07 10:39:23 +0100

    ADD redirect and alert on error

--- pwa/src/pages/sessions/[sessionId].tsx
+++ pwa/src/pages/sessions/[sessionId].tsx
@@ -1,14 +1,17 @@
 import { Card } from "@conductionnl/nl-design-system";
+import { navigate } from "gatsby-link";
 import * as React from "react";
 import APIContext from "../../apiService/apiContext";
 import APIService from "../../apiService/apiService";
 import Spinner from "../../components/common/spinner";
 import LogsTable from "../../components/logs/logTable/logTable";
+import { AlertContext } from "../../context/alertContext";
 import { HeaderContext } from "../../context/headerContext";
 
 const IndexPage = (props) => {
   const [logs, setLogs] = React.useState(null);
   const [_, setHeader] = React.useContext(HeaderContext);
+  const [__, setAlert] = React.useContext(AlertContext);
 
   const API: APIService = React.useContext(APIContext);
   const sessionId: string = props.params.sessionId;
@@ -23,6 +26,8 @@ const IndexPage = (props) => {
         setLogs(res.data);
       })
       .catch((err) => {
+        navigate("/");
+        setAlert({ message: "Error getting session logs", type: "danger" });
         throw new Error(`GET All Logs error: ${err}`);
       });
   }, [API]);
