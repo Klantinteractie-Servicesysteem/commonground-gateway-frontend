0a6ce376da6a680aed47c62f78841b4a8002c175
Remko <remko@conduction.nl>
2022-03-09 10:12:15 +0100

    right align cancel button

--- pwa/src/components/deleteModal/DeleteModal.tsx
+++ pwa/src/components/deleteModal/DeleteModal.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import "./deletemodal.css";
+import "./deleteModal.css";
 import { Modal } from "@conductionnl/nl-design-system";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
@@ -27,6 +27,7 @@ const DeleteModal: React.FC<DeleteModalProps> = ({ handleDelete, handleId, optio
               ) : (
                 <p>Are you sure you want to delete {handleId}?</p>
               )}
+              <br></br>
               <button
                 className="utrecht-button btn-sm btn-danger mr-2"
                 onClick={() => handleDelete(handleId)}
@@ -34,8 +35,8 @@ const DeleteModal: React.FC<DeleteModalProps> = ({ handleDelete, handleId, optio
               >
                 <FontAwesomeIcon icon={faTrash} /> Delete
               </button>
-              <button className="utrecht-button btn-sm mr-2" data-bs-dismiss="modal">
-                Cancel
+              <button className="utrecht-button cancel-btn-right btn-sm mr-2" data-bs-dismiss="modal">
+                Cancel  
               </button>
             </>
           );
