7b9f0ba2eb515ba9d4c0f9f0b528991c32b66943
Lennart <66552932+lencodes@users.noreply.github.com>
2022-03-17 13:41:39 +0100

    ADD visual required feedback in InputGroup

--- pwa/src/components/formFields/formGroup.tsx
+++ pwa/src/components/formFields/formGroup.tsx
@@ -5,11 +5,15 @@ interface IInputGroupProps {
   name: string;
   label: string;
   errors: FieldErrors;
+  required?: boolean;
 }
 
 export const InputGroup: React.FC<IInputGroupProps> = (props) => (
   <div className="FormField-group">
-    <label htmlFor={props.name}>{props.label}</label>
+    <label htmlFor={props.name}>
+      {props.label}
+      {props.required && <b>*</b>}
+    </label>
     {props.children}
     {props.errors[props.name] && props.errors[props.name].message}
   </div>
