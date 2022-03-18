70fcd1a98514105a4b38b3d5b4f2d393cf1104f4
Lennart <66552932+lencodes@users.noreply.github.com>
2022-03-18 09:44:47 +0100

    ADD 5 minutes global staleTime

--- pwa/gatsby-ssr.js
+++ pwa/gatsby-ssr.js
@@ -20,7 +20,9 @@ export const onRouteUpdate = () => {
 };
 
 export const wrapRootElement = ({ element }) => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: { queries: { staleTime: 5 * 60 * 1000 } },
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
