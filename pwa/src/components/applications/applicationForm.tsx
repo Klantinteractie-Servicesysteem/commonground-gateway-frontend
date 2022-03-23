64fb16c7287c9c801651a2c6e2f1dca67972ddb9
Hamid <gh.hamid89@gmail.com>
2022-03-23 13:02:15 +0100

    React-hook-form implementatie: Application en Queries

--- pwa/src/components/applications/applicationForm.tsx
+++ pwa/src/components/applications/applicationForm.tsx
@@ -1,97 +1,137 @@
 import * as React from "react";
-import {
-  GenericInputComponent,
-  TextareaGroup,
-  Spinner,
-  Card,
-  Accordion,
-  Modal,
-} from "@conductionnl/nl-design-system/lib";
+import { Spinner, Card, Accordion, Modal } from "@conductionnl/nl-design-system/lib";
 import { Link } from "gatsby";
 import { navigate } from "gatsby-link";
-import {
-  checkValues,
-  removeEmptyObjectValues,
-  retrieveFormArrayAsOArray,
-  retrieveFormArrayAsOArrayWithName,
-} from "../utility/inputHandler";
 import ElementCreationNew from "../common/elementCreationNew";
 import APIService from "../../apiService/apiService";
 import APIContext from "../../apiService/apiContext";
 import LoadingOverlay from "../loadingOverlay/loadingOverlay";
 import { HeaderContext } from "../../context/headerContext";
 import { AlertContext } from "../../context/alertContext";
-import MultiSelect from "../common/multiSelect";
-import { useQuery } from "react-query";
-
-interface IApplication {
-  name: string;
-  description: string;
-  public: string;
-  secret: string;
-  resource: string;
-  domains: Array<string>;
-  endpoints: any;
-}
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { useForm } from "react-hook-form";
+import { InputText, SelectMultiple, Textarea } from "../formFields";
 
 interface ApplicationFormProps {
-  id?: string;
+  applicationId?: string;
 }
 
-export const ApplicationForm: React.FC<ApplicationFormProps> = ({ id }) => {
-  const [application, setApplication] = React.useState<IApplication>(null);
+export const ApplicationForm: React.FC<ApplicationFormProps> = ({ applicationId }) => {
   const [endpoints, setEndpoints] = React.useState<any>(null);
-  const [showSpinner, setShowSpinner] = React.useState<boolean>(false);
   const [loadingOverlay, setLoadingOverlay] = React.useState<boolean>(false);
+  const title: string = applicationId ? "Edit Application" : "Create Application";
   const API: APIService = React.useContext(APIContext);
-  const title: string = id ? "Edit Application" : "Create Application";
   const [documentation, setDocumentation] = React.useState<string>(null);
   const [_, setAlert] = React.useContext(AlertContext);
   const [__, setHeader] = React.useContext(HeaderContext);
 
-  const getEndpointsSelectQuery = useQuery<any[], Error>("endpoints-select", API.Endpoint.getSelect, {
+  /**
+   * Form fields and logic
+   */
+  const fields = ["name", "resource", "public", "secret", "description", "endpoints"];
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    setValue,
+    control,
+  } = useForm();
+
+  const onSubmit = (data): void => {
+    data.endpoints = data.endpoints?.map((endpoints) => endpoints.value);
+    createOrEditApplication.mutate({ payload: data, id: applicationId });
+  };
+
+  /**
+   * Queries and mutations
+   */
+  const queryClient = useQueryClient();
+
+  const getApplication = useQuery<any, Error>(
+    ["Applications", applicationId],
+    () => API.Endpoint.getOne(applicationId),
+    {
+      initialData: () =>
+        queryClient.getQueryData<any[]>("Applications")?.find((application) => application.id === applicationId),
+      onError: (error) => {
+        setAlert({ message: error.message, type: "danger" });
+      },
+      enabled: !!applicationId,
+    },
+  );
+
+  const createOrEditApplication = useMutation<any, Error, any>(API.Application.createOrUpdate, {
+    onMutate: () => {
+      setLoadingOverlay(true);
+    },
+    onSuccess: async (newApplication) => {
+      const previousApplications = queryClient.getQueryData<any[]>("applications");
+      await queryClient.cancelQueries("applications");
+
+      if (applicationId) {
+        const index = previousApplications.findIndex((application) => application.id === applicationId);
+        previousApplications[index] = newApplication;
+        queryClient.setQueryData("applications", previousApplications);
+        queryClient.setQueryData(["applications", applicationId], newApplication);
+      }
+
+      if (!applicationId) {
+        queryClient.setQueryData("applications", [newApplication, ...previousApplications]);
+        queryClient.setQueryData(["applications", newApplication.id], newApplication);
+      }
+
+      queryClient.invalidateQueries("applications");
+      setAlert({ message: `${applicationId ? "Updated" : "Created"} application`, type: "success" });
+      navigate("/applications");
+    },
     onError: (error) => {
       setAlert({ message: error.message, type: "danger" });
     },
+    onSettled: () => {
+      setLoadingOverlay(false);
+    },
   });
 
+  /**
+   * Effects
+   */
   React.useEffect(() => {
-    setHeader(
-      <>
-        Application <i>{application && application.name}</i>
-      </>,
-    );
-  }, [setHeader, application]);
+    setHeader("Endpoint");
+
+    if (getApplication.isSuccess) {
+      setHeader(
+        <>
+          Application: <i>{getApplication.data.name}</i>
+        </>,
+      );
+
+      fields.map((field) => {
+        setValue(field, getApplication.data[field]);
+      });
+    }
+  }, [getApplication.isSuccess]);
 
   React.useEffect(() => {
-    handleSetDocumentation();
-  });
+    handleSetEndpoints();
+  }, [API, applicationId]);
 
-  React.useEffect(() => {
-    id && handleSetApplications();
-  }, [API, id]);
-
-  const handleSetApplications = () => {
-    setShowSpinner(true);
-
-    API.Application.getOne(id)
+  const handleSetEndpoints = () => {
+    API.Endpoint.getAll()
       .then((res) => {
-        res.data.endpoints = res.data.endpoints.map((endpoint) => {
-          return { name: endpoint.name, id: endpoint.name, value: `/admin/endpoints/${endpoint.id}` };
+        const _endpoints = res.data?.map((endpoint) => {
+          return { label: endpoint.name, value: `/admin/applications/${endpoint.id}` };
         });
-        setApplication(res.data);
+        setEndpoints(_endpoints);
       })
       .catch((err) => {
         setAlert({ message: err, type: "danger" });
-        throw new Error("GET application error: " + err);
-      })
-      .finally(() => {
-        setShowSpinner(false);
+        throw new Error("GET endpoint error: " + err);
       });
   };
 
   const handleSetDocumentation = (): void => {
-    API.Documentation.get("applications")
+    API.Documentation.get("endpoints")
       .then((res) => {
         setDocumentation(res.data.content);
       })
@@ -101,61 +141,22 @@ export const ApplicationForm: React.FC<ApplicationFormProps> = ({ id }) => {
       });
   };
 
-  const saveApplication = (event) => {
-    event.preventDefault();
-    setLoadingOverlay(true);
-
-    let domains = retrieveFormArrayAsOArray(event.target, "domains");
-    let endpoints = retrieveFormArrayAsOArrayWithName(event.target, "endpoints");
-
-    let body: any = {
-      name: event.target.name.value,
-      description: event.target.description ? event.target.description.value : null,
-      public: event.target.public.value ? event.target.public.value : null,
-      secret: event.target.secret.value ? event.target.secret.value : null,
-      resource: event.target.resource.value ? event.target.resource.value : null,
-      domains,
-      endpoints,
-    };
-
-    body = removeEmptyObjectValues(body);
-
-    if (!checkValues([body.name, body.domains])) {
-      setAlert({ type: "danger", message: "Required fields are empty" });
-      setLoadingOverlay(false);
-      return;
-    }
-
-    API.Application.createOrUpdate(body, id)
-      .then(() => {
-        setAlert({ message: `${id ? "Updated" : "Created"} application`, type: "success" });
-        navigate("/applications");
-      })
-      .catch((err) => {
-        setAlert({ type: "danger", message: err.message });
-        throw new Error(`Create or update application error ${err}`);
-      })
-      .finally(() => {
-        setLoadingOverlay(false);
-      });
-  };
-
   return (
-    <form id="applicationForm" onSubmit={saveApplication}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Card
         title={title}
         cardHeader={function () {
           return (
-            <>
-              <button
+            <div>
+              <a
                 className="utrecht-link button-no-style"
                 data-bs-toggle="modal"
                 data-bs-target="#applicationHelpModal"
-                onClick={(e) => e.preventDefault()}
+                onClick={!documentation && handleSetDocumentation}
               >
                 <i className="fas fa-question mr-1" />
                 <span className="mr-2">Help</span>
-              </button>
+              </a>
               <Modal
                 title="Application Documentation"
                 id="applicationHelpModal"
@@ -171,67 +172,37 @@ export const ApplicationForm: React.FC<ApplicationFormProps> = ({ id }) => {
                 <i className="fas fa-save mr-2" />
                 Save
               </button>
-            </>
+            </div>
           );
         }}
         cardBody={function () {
           return (
             <div className="row">
               <div className="col-12">
-                {showSpinner === true ? (
+                {getApplication.isLoading ? (
                   <Spinner />
                 ) : (
                   <div>
                     {loadingOverlay && <LoadingOverlay />}
                     <div className="row form-row">
                       <div className="col-6">
-                        <GenericInputComponent
-                          type={"text"}
-                          name={"name"}
-                          id={"nameInput"}
-                          data={application && application.name && application.name}
-                          nameOverride={"Name"}
-                          required
-                        />
+                        <InputText label="Name" name="name" {...{ register, errors }} validation={{ required: true }} />
                       </div>
                       <div className="col-6">
-                        <GenericInputComponent
-                          type={"text"}
-                          name={"resource"}
-                          id={"resourceInput"}
-                          data={application && application.resource && application.resource}
-                          nameOverride={"Resource"}
-                        />
+                        <InputText label="Resource" name="resource" {...{ register, errors }} />
                       </div>
                     </div>
                     <div className="row form-row">
                       <div className="col-6">
-                        <GenericInputComponent
-                          type={"text"}
-                          name={"public"}
-                          id={"publicInput"}
-                          data={application && application.public && application.public}
-                          nameOverride={"Public"}
-                        />
+                        <InputText label="Public" name="public" {...{ register, errors }} />
                       </div>
                       <div className="col-6">
-                        <GenericInputComponent
-                          type={"text"}
-                          name={"secret"}
-                          id={"secretInput"}
-                          data={application && application.secret && application.secret}
-                          nameOverride={"Secret"}
-                        />
+                        <InputText label="Secret" name="secret" {...{ register, errors }} />
                       </div>
                     </div>
                     <div className="row form-row">
                       <div className="col-6">
-                        <TextareaGroup
-                          label="Description"
-                          name={"description"}
-                          id={"descriptionInput"}
-                          defaultValue={application?.description}
-                        />
+                        <Textarea label="Description" name="description" {...{ register, errors }} />
                       </div>
                     </div>
                     <Accordion
@@ -241,19 +212,19 @@ export const ApplicationForm: React.FC<ApplicationFormProps> = ({ id }) => {
                           title: "Domains *",
                           id: "domainsAccordion",
                           render: function () {
-                            return <ElementCreationNew id="domains" label="Domains" data={application?.domains} />;
+                            return <ElementCreationNew id="domains" label="Domains" />;
                           },
                         },
                         {
                           title: "Endpoints",
                           id: "endpointsAccordion",
                           render: function () {
-                            return getEndpointsSelectQuery.isSuccess ? (
-                              <MultiSelect
-                                id=""
-                                label="endpoints"
-                                data={application?.endpoints}
-                                options={getEndpointsSelectQuery.data}
+                            return endpoints ? (
+                              <SelectMultiple
+                                label="Endpoints"
+                                name="endpoints"
+                                options={endpoints}
+                                {...{ control, register, errors }}
                               />
                             ) : (
                               <Spinner />
