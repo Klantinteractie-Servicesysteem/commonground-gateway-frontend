c428f6b16adf36469544e61bdfd76be99f68be3f
Barry Brands <barrybrands02@hotmail.com>
2022-03-29 17:35:40 +0200

    valdiateJSON improvement

--- pwa/src/services/validateJSON.ts
+++ pwa/src/services/validateJSON.ts
@@ -1,12 +1,12 @@
-export const validateJSON = (_json: string): boolean => {
+export const validateJSON = (_json: string): boolean|string => {
   const RegExp: RegExp = /^[0-9]+$/;
 
-  if (RegExp.test(_json)) return false;
+  if (RegExp.test(_json)) return 'Invalid JSON';
 
   try {
     JSON.parse(_json);
     return true;
   } catch {
-    return false;
+    return 'Invalid JSON';
   }
 };
