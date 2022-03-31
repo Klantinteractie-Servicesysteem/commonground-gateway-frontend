66e9f633b816e9b7719e0d017ad6693903c404d9
Lennart <66552932+lencodes@users.noreply.github.com>
2022-03-31 15:42:20 +0200

    CLEANUP

--- pwa/src/components/formFields/createKeyValue/createKeyValue.tsx
+++ pwa/src/components/formFields/createKeyValue/createKeyValue.tsx
@@ -70,10 +70,6 @@ const KeyValueComponent: React.FC<CreateKeyValueComponentProps> = ({ data, handl
     handleChange(keyValues);
   }, [keyValues]);
 
-  React.useEffect(() => {
-    setKeyValues(data);
-  }, [data]);
-
   return (
     <div className="KeyValue">
       {keyValues && (
