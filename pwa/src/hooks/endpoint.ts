1c957da56eff7f87662c0596369126c5e6e19f9a
Lennart <66552932+lencodes@users.noreply.github.com>
2022-04-01 08:09:10 +0200

    IMPLEMENT generic service in endpoint hook

--- pwa/src/hooks/endpoint.ts
+++ pwa/src/hooks/endpoint.ts
@@ -5,6 +5,7 @@ import APIContext from "../apiService/apiContext";
 import { AlertContext } from "../context/alertContext";
 import { LoadingOverlayContext } from "../context/loadingOverlayContext";
 import { navigate } from "gatsby-link";
+import { addItem, deleteItem, updateItem } from "../services/mutateQueries";
 
 export const useEndpoint = (queryClient: QueryClient) => {
   const API: APIService = React.useContext(APIContext);
@@ -32,18 +33,12 @@ export const useEndpoint = (queryClient: QueryClient) => {
       onMutate: () => {
         setLoadingOverlay({ isLoading: true });
       },
+      onSuccess: async (_, variables) => {
+        deleteItem(queryClient, "endpoints", variables.id);
+        setAlert({ message: "Deleted endpoint", type: "success" });
+      },
       onError: (error) => {
         setAlert({ message: error.message, type: "danger" });
-      },
-      onSuccess: async (_, variables) => {
-        const previousEndpoints = queryClient.getQueryData<any[]>("endpoints");
-        await queryClient.cancelQueries("endpoints");
-
-        const newEndpoints = previousEndpoints.filter((endpoint) => endpoint.id !== variables.id);
-        queryClient.setQueryData("endpoints", [...newEndpoints]);
-
-        queryClient.invalidateQueries("endpoints");
-        setAlert({ message: "Deleted endpoint", type: "success" });
       },
       onSettled: () => {
         setLoadingOverlay({ isLoading: false });
@@ -56,27 +51,16 @@ export const useEndpoint = (queryClient: QueryClient) => {
         setLoadingOverlay({ isLoading: true });
       },
       onSuccess: async (newEndpoint) => {
-        await queryClient.cancelQueries("endpoints");
-        const previousEndpoints = queryClient.getQueryData<any[]>("endpoints");
-
-        if (!previousEndpoints) {
-          endpointId && queryClient.setQueryData(["endpoints", endpointId], newEndpoint);
-          !endpointId && queryClient.setQueryData(["endpoints", newEndpoint.id], newEndpoint);
+        if (endpointId) {
+          updateItem(queryClient, "endpoints", newEndpoint);
+          setAlert({ message: "Updated endpoint", type: "success" });
         }
 
-        if (previousEndpoints && endpointId) {
-          const index = previousEndpoints.findIndex((endpoint) => endpoint.id === endpointId);
-          previousEndpoints[index] = newEndpoint;
-          queryClient.setQueryData("endpoints", previousEndpoints);
+        if (!endpointId) {
+          addItem(queryClient, "endpoints", newEndpoint);
+          setAlert({ message: "Created endpoint", type: "success" });
         }
 
-        if (previousEndpoints && !endpointId) {
-          queryClient.setQueryData("endpoints", [newEndpoint, ...previousEndpoints]);
-        }
-
-        queryClient.invalidateQueries("endpoints");
-
-        setAlert({ message: `${endpointId ? "Updated" : "Created"} endpoint`, type: "success" });
         navigate("/endpoints");
       },
       onError: (error) => {
