a05be7093110b02669bd9b8348c60bc23fb568b1
Lennart <66552932+lencodes@users.noreply.github.com>
2022-04-02 10:44:08 +0200

    REFACTOR Log API Serivce to be compatible with react-hook

--- pwa/src/apiService/resources/log.ts
+++ pwa/src/apiService/resources/log.ts
@@ -1,5 +1,5 @@
 import { Send } from "../apiService";
-import { AxiosInstance, AxiosResponse } from "axios";
+import { AxiosInstance } from "axios";
 
 export default class Log {
   private _instance: AxiosInstance;
@@ -8,31 +8,38 @@ export default class Log {
     this._instance = _instance;
   }
 
-  public getAllIncoming = (): Promise<AxiosResponse> => {
-    return Send(this._instance, "GET", "/logs?type=in");
+  public getAllIncoming = async (): Promise<any> => {
+    const { data } = await Send(this._instance, "GET", "/logs?type=in");
+    return data;
   };
 
-  public getAllFromSession = (sessionId: string): Promise<AxiosResponse> => {
-    return Send(this._instance, "GET", `/logs?session=${sessionId}`);
+  public getAllFromSession = async (sessionId: string): Promise<any> => {
+    const { data } = await Send(this._instance, "GET", `/logs?session=${sessionId}`);
+    return data;
   };
 
-  public getAllFromCall = (callId: string): Promise<AxiosResponse> => {
-    return Send(this._instance, "GET", `/logs?callId=${callId}`);
+  public getAllFromCall = async (callId: string): Promise<any> => {
+    const { data } = await Send(this._instance, "GET", `/logs?callId=${callId}`);
+    return data;
   };
 
-  public getAllFromEntity = (entityId: string): Promise<AxiosResponse> => {
-    return Send(this._instance, "GET", `/logs?entity.id=${entityId}`);
+  public getAllFromEntity = async (entityId: string): Promise<any> => {
+    const { data } = await Send(this._instance, "GET", `/logs?entity.id=${entityId}`);
+    return data;
   };
 
-  public getAllFromEndpoint = (endpointId: string): Promise<AxiosResponse> => {
-    return Send(this._instance, "GET", `/logs?endpoint.id=${endpointId}`);
+  public getAllFromEndpoint = async (endpointId: string): Promise<any> => {
+    const { data } = await Send(this._instance, "GET", `/logs?endpoint.id=${endpointId}`);
+    return data;
   };
 
-  public getAllFromSource = (sourceId: string): Promise<AxiosResponse> => {
-    return Send(this._instance, "GET", `/logs?source.id=${sourceId}`);
+  public getAllFromSource = async (sourceId: string): Promise<any> => {
+    const { data } = await Send(this._instance, "GET", `/logs?source.id=${sourceId}`);
+    return data;
   };
 
-  public getAllOutgoingFromCallId = (callId: string): Promise<AxiosResponse> => {
-    return Send(this._instance, "GET", `/logs?type=out&callId=${callId}`);
+  public getAllOutgoingFromCallId = async (callId: string): Promise<any> => {
+    const { data } = await Send(this._instance, "GET", `/logs?type=out&callId=${callId}`);
+    return data;
   };
 }
