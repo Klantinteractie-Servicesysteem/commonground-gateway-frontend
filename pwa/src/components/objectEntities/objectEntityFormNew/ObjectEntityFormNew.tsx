9eb39d0a6afe8b76bbeae539287f3726472049b9
Barry Brands <barrybrands02@hotmail.com>
2022-04-04 16:41:06 +0200

    ObjectEntityFormNew cleanup

--- pwa/src/components/objectEntities/objectEntityFormNew/ObjectEntityFormNew.tsx
+++ pwa/src/components/objectEntities/objectEntityFormNew/ObjectEntityFormNew.tsx
@@ -15,96 +15,39 @@ export const ObjectEntityFormNew: React.FC<ObjectEntityFormNewProps> = ({ object
   const API: APIService = React.useContext(APIContext);
   const [entity, setEntity] = React.useState(null);
   const [object, setObject] = React.useState(null);
-  const [objectAPI, setObjectAPI] = React.useState(null);
-  const [showSpinner, setShowSpinner] = React.useState(null);
+  const [showSpinner, setShowSpinner] = React.useState(true);
   const [formIOSchema, setFormIOSchema] = React.useState(null);
   const [formIO, setFormIO] = React.useState(null);
   const [_, setAlert] = React.useContext(AlertContext);
   const title = objectId ?? "New object";
 
   React.useEffect(() => {
-    getEntity();
-    // objectId && getObject();
+    if (!entity) {
+      console.log(entity);
+      getEntity();
+    }
   }, [API]);
+
   React.useEffect(() => {
-    entity && getFormIOSchema();
-    // entity && getObjectAPI();
+    entity && !formIOSchema && getFormIOSchema();
   }, [entity]);
 
   React.useEffect(() => {
-    if ((!entity || !formIO) && !showSpinner) {
-      setShowSpinner(true);
-      return;
-    }
-    entity && formIO && showSpinner && setShowSpinner(false);
+    !entity || !formIO ? setShowSpinner(true) : setShowSpinner(false);
   }, [entity, formIO]);
 
   React.useEffect(() => {
-    if (!formIOSchema) return;
-
-    import("@formio/react").then((formio) => {
-      const { Form } = formio;
-      setFormIO(<Form src={formIOSchema} onSubmit={saveObject} options={{ noAlerts: false }} />);
-    });
+    formIOSchema &&
+      import("@formio/react").then((formio) => {
+        const { Form } = formio;
+        setFormIO(<Form src={formIOSchema} onSubmit={saveObject} options={{ noAlerts: false }} />);
+      });
   }, [formIOSchema]);
 
-  // const getObject = () => {
-  //   setObject(null);
-  //   let id: string;
-  //   objectId ? (id = objectId) : (id = null);
-  //   !id && object && object.id && (id = object.id);
-
-  //   if (id) {
-  //     API.ObjectEntity.getOne(id)
-  //       .then((res) => {
-  //         console.log(res.data.objectValues[0]);
-  //         setObject(res.data);
-  //       })
-  //       .catch((err) => {
-  //         throw new Error("GET objectEntity error: " + err);
-  //       })
-  //       .finally(() => {
-  //         setShowSpinner(false);
-  //       });
-  //   }
-  // };
-
-  // const getObjectAPI = () => {
-  //   setObjectAPI(null);
-  //   let id: string;
-  //   objectId ? (id = objectId) : (id = null);
-  //   !id && object && object.id && (id = object.id);
-
-  //   console.log(entity.handlers[0].endpoints[0].path.join("/") + id);
-  //   let endpointForCall;
-  //   entity.handlers[0].endpoints.forEach((key, endpoint) => {
-  //     if (endpoint.operationType === "collection" && (endpoint.method === "GET" || endpoint.method === "get")) {
-  //       endpointForCall = endpoint;
-  //     }
-  //   });
-
-  //   let url = "weer";
-
-  //   if (id) {
-  //     API.ApiCalls.getOne(url, id)
-  //       .then((res) => {
-  //         console.log(res);
-  //         setObjectAPI(res);
-  //       })
-  //       .catch((err) => {
-  //         throw new Error("GET objectAPI error: " + err);
-  //       })
-  //       .finally(() => {
-  //         setShowSpinner(false);
-  //       });
-  //   }
-  // };
-
   const getEntity = () => {
-    setEntity(null);
     API.Entity.getOne(entityId)
       .then((res) => {
-        setEntity(res.data);
+        setEntity(res);
       })
       .catch((err) => {
         throw new Error("GET entity error: " + err);
@@ -112,20 +55,36 @@ export const ObjectEntityFormNew: React.FC<ObjectEntityFormNewProps> = ({ object
   };
 
   const getFormIOSchema = () => {
-    if (formIOSchema) return;
-    setFormIOSchema(null);
     let schemaUrl;
     let endpointForCall;
-    entity.handlers[0].endpoints.forEach((key, endpoint) => {
-      if (endpoint.operationType === "collection" && (endpoint.method === "GET" || endpoint.method === "get")) {
+    entity.handlers[0].endpoints.forEach((endpoint, key) => {
+      if (
+        endpoint.operationType === (objectId ? "item" : "collection") &&
+        (endpoint.method === "GET" || endpoint.method === "get")
+      ) {
         endpointForCall = endpoint;
       }
     });
-    // schemaUrl = endpointForCall.path.join("/") + (objectId ? "/" + objectId : "");
-    schemaUrl = "weer/" + objectId;
+
+    schemaUrl = endpointForCall?.path?.join("/");
+    if (schemaUrl === null || schemaUrl === undefined) {
+      setAlert({
+        message: `No Endpoint found with method: GET and operationType: ${
+          objectId ? "item" : "collection"
+        }. Check if this Entity has a Handler that has an Endpoint with method: GET and operationType: ${
+          objectId ? "item" : "collection"
+        }`,
+        type: "danger",
+      });
+      return;
+    }
+
+    if (schemaUrl.includes("{id}")) {
+      schemaUrl = schemaUrl.replace("{id}", objectId);
+    }
+
     API.FormIO.getSchema(schemaUrl)
       .then((res) => {
-        console.log(res.data);
         setFormIOSchema(res.data);
       })
       .catch((err) => {
@@ -133,60 +92,8 @@ export const ObjectEntityFormNew: React.FC<ObjectEntityFormNewProps> = ({ object
       });
   };
 
-  // const fillFormIOSchema = (schema: any) => {
-  //   let schemaWithData = schema;
-  //   let filledAttributes = [];
-  //   const advConfigData = {
-  //     "@application": object.application ? `/admin/applications/${object.application.id}` : null,
-  //     "@organization": object.organization ?? null,
-  //     "@owner": object.owner ?? null,
-  //   };
-  //   for (let i = 0; i < schemaWithData.components.length; i++) {
-  //     // Set advanced configuration defaultValues
-  //     if (schemaWithData.components[i].key === "advancedConfiguration") {
-  //       filledAttributes.push(schemaWithData.components[i].key);
-  //       for (let k = 0; k < schemaWithData.components[i].components.length; k++) {
-  //         if (filledAttributes.includes(schemaWithData.components[i].components[k].key)) {
-  //           continue;
-  //         }
-  //         if (advConfigData[schemaWithData.components[i].components[k].key]) {
-  //           schemaWithData.components[i].components[k].defaultValue =
-  //             advConfigData[schemaWithData.components[i].components[k].key];
-  //           continue;
-  //         }
-  //       }
-  //     }
-  //     // Set defaultValues other attributes
-  //     for (let j = 0; j < object?.objectValues?.length; j++) {
-  //       if (filledAttributes.includes(object?.objectValues[j]?.attribute.name)) {
-  //         continue;
-  //       }
-  //       // Fill entity attributes
-  //       if (
-  //         schemaWithData.components[i].type === "panel" &&
-  //         schemaWithData.components[i].key !== "advancedConfiguration"
-  //       ) {
-  //         schemaWithData.components[i].components.forEach((component, key) => {
-  //           console.log(component.key);
-  //         });
-  //       }
-  //       // Fill normal attribute
-  //       if (schemaWithData.components[i].key === object.objectValues[j]?.attribute.name) {
-  //         let type = object.objectValues[j]?.attribute.type;
-  //         schemaWithData.components[i].defaultValue = object.objectValues[j][`${type}Value`];
-  //         filledAttributes.push(schemaWithData.components[i].key);
-  //         break;
-  //       }
-  //     }
-  //   }
-  //   return schemaWithData;
-  // };
-
   const saveObject = (event) => {
-    // console.log(event);
-    // return;
     setShowSpinner(true);
-    setObject(null);
     let body = event.data;
     body.submit = undefined;
 
@@ -209,7 +116,7 @@ export const ObjectEntityFormNew: React.FC<ObjectEntityFormNewProps> = ({ object
         })
         .finally(() => {
           setAlert({ message: "Saved object", type: "success" });
-          // getObject();
+          setShowSpinner(false);
         });
     }
   };
@@ -246,9 +153,9 @@ export const ObjectEntityFormNew: React.FC<ObjectEntityFormNewProps> = ({ object
       cardBody={function () {
         return (
           <>
-            {!formIO && <Spinner />}
+            {showSpinner && <Spinner />}
             {formIO && formIO}
-            {!showSpinner && formIO && (
+            {formIO && (
               <>
                 {/* <Accordion
                   id="errorsAccordion"
