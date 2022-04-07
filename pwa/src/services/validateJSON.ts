127f231e1993f0be6b2f853f4d7b18d325e659db
Remko <remko@conduction.nl>
2022-04-07 10:41:42 +0200

    added charaacter length validation to json

--- pwa/src/services/validateJSON.ts
+++ pwa/src/services/validateJSON.ts
@@ -1,10 +1,13 @@
 export const validateJSON = (data: string): boolean | string => {
   const RegExp: RegExp = /^[0-9]+$/;
   const invalidMessage = "This field contains invalid JSON";
+  const invalidLengthMessage = "Field contains too many characters"
 
   if (!data) return true; // empty data should not be validated
 
   if (RegExp.test(data)) return invalidMessage;
+
+  if (data.length > 255) return invalidLengthMessage;
 
   try {
     return JSON.parse(data) && true;
